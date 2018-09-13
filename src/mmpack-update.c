7855a30a02759cb5c2251c0fd52f93ab5d41cc5e
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2018-09-13 12:20:11 +0200

    Remove implementation of mmpack-update
    The command was not working properly and it is being reimplemented
    differently. The code is disabled to ease the reimplementation of some
    of the components it uses (config parsing among other).
    
    Change-Id: I1bb371e7a96721d3da1ee78ccc6604e1a8f22d96

--- src/mmpack-update.c
+++ src/mmpack-update.c
@@ -6,40 +6,13 @@
 # include <config.h>
 #endif
 
-#include <curl/curl.h>
-#include <mmerrno.h>
-
 #include "context.h"
-#include "mmpack-config.h"
 #include "mmpack-update.h"
-#include "utils.h"
-
-static
-int mmpack_update(char const * server, char const * url, void * arg)
-{
-	CURLcode res;
-	struct mmpack_ctx * ctx = (struct mmpack_ctx *) arg;
-
-	if (ctx->curl == NULL) {
-		ctx->curl = curl_easy_init();
-		if (ctx->curl == NULL)
-			return mm_raise_from_errno("curl init failed");
-	}
-
-	curl_easy_setopt(ctx->curl, CURLOPT_URL, url);
-	curl_easy_setopt(ctx->curl, CURLOPT_FOLLOWLOCATION, 1L);
-
-	res = curl_easy_perform(ctx->curl);
-	if(res != CURLE_OK)
-		return mm_raise_from_errno("update from %s failed: %s",
-				server, curl_easy_strerror(res));
-
-	return 0;
-}
-
 
 LOCAL_SYMBOL
 int mmpack_update_all(struct mmpack_ctx * ctx)
 {
-	return foreach_config_server(get_config_filename(), mmpack_update, ctx);
+	(void)ctx;
+
+	return -1;
 }
