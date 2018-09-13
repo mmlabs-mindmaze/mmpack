72b1788239b28b2217db4fa85c0791126bcb771a
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2018-09-13 16:47:47 +0200

    Reimplement update command
    Change-Id: I772ffa5df6d569987d1223c3d8894884e2aa64e7

--- src/mmpack-update.c
+++ src/mmpack-update.c
@@ -7,12 +7,29 @@
 #endif
 
 #include "context.h"
+#include "download.h"
 #include "mmpack-update.h"
+#include "mmstring.h"
+#include "utils.h"
+
 
 LOCAL_SYMBOL
 int mmpack_update_all(struct mmpack_ctx * ctx)
 {
-	(void)ctx;
+	STATIC_CONST_MMSTR(cacheindex, REPO_INDEX_RELPATH)
+	STATIC_CONST_MMSTR(pkglist, "binary-index")
+	const mmstr* url = ctx->settings.repo_url;
+	const mmstr* prefix = ctx->prefix;
 
-	return -1;
+	if (!url) {
+		fprintf(stderr, "Repository URL unspecified\n");
+		return -1;
+	}
+
+	if (download_from_repo(ctx, url, pkglist, prefix, cacheindex)) {
+		fprintf(stderr, "Failed to download package list\n");
+		return -1;
+	}
+
+	return 0;
 }
