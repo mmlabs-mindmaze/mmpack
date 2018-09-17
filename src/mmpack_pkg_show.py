e9bab9b58d6febf354097f757e582bef8e556d1e
Gabriel Ganne <gabriel.ganne@mindmaze.ch>
2018-09-17 13:53:26 +0200

    mmpack-build pkg-show <binpkg>
    This is intended to be a helper for package creation.
    
    It works with binpkg being the full path to a mpk file, or by giving the
    package name, in which case, the dull package path is
    reconstructed/guessed from within xdg_data_dir, pattern ".*<binpkg>.*\.mpk"
    
    The package summary and files are then dumped on the screen.
    
    Change-Id: Idce69e6291f57c36b5f33cbb1a7d9043c0839a65

--- src/mmpack_pkg_show.py
+++ src/mmpack_pkg_show.py
@@ -5,20 +5,63 @@ This is not mmpack-info.py
 could be aliased with mmpack-pkg-info.py
 '''
 
+import os
 import sys
+import tarfile
+from glob import glob
+from common import pushdir, popdir
+from workspace import Workspace
 
 
-def usage(progname):
-    'show usage'
-    print('usage {}'.format(progname))
+def _usage(progname):
+    print('usage: {} <pkg name>'.format(progname))
+
+
+def _is_primary(pkgname: str) -> bool:
+    return not (pkgname.endswith('-devel')
+                and pkgname.endswith('-pkg')
+                and pkgname.endswith('-debug'))
+
+
+def _get_pkg_fullpath(pkgname: str) -> str:
+    if os.path.isfile(pkgname):
+        return pkgname
+    else:
+        pkgdir = Workspace().packages + '/'
+        pushdir(pkgdir)
+        candidates = glob('*' + pkgname + '*.mpk')
+        popdir()
+        candidates = [x for x in candidates if _is_primary(x)]
+        if not candidates:
+            return None
+
+        pkgname = candidates[0]
+        if len(candidates) != 1:
+            print('[guess] full package name: {0}'.format(pkgname),
+                  file=sys.stderr)
+        fullpath = pkgdir + pkgname
+        if os.path.isfile(fullpath):
+            return fullpath
+        raise FileNotFoundError(pkgname + ' not found !')
 
 
 def main():
     'TODOC'
-    try:
-        raise IndexError
-    except IndexError:
-        return usage(sys.argv[0])
+    pkgname = sys.argv[1]
+    full_pkg_path = _get_pkg_fullpath(pkgname)
+    if not full_pkg_path:
+        return 0
+    pkg_archive = tarfile.open(full_pkg_path, 'r:xz')
+
+    # package metadatas
+    info = pkg_archive.extractfile('./MMPACK/info')
+    msg = info.read().decode('utf-8')
+    print(msg)
+
+    print('Files:')
+    for file in pkg_archive.getnames():
+        if not file.startswith('./MMPACK'):
+            print('\t' + file)
 
 
 if __name__ == '__main__':
