35df9a77162954cf89c926050c48fe149cfc6ae0
Gabriel Ganne <gabriel.ganne@mindmaze.ch>
2018-10-08 09:31:36 +0200

    add PY_RUN_CASE to filter pyton tests
    !! filters on test filenames, not test names !!
    
    eg.
        skip all tests: make check CK_RUN_CASE=none PY_RUN_CASE=none
        only run python version tests: make check CK_RUN_CASE=none PY_RUN_CASE=version
    
    Change-Id: Ib106899e7c05982295b3896c30e14902e35c9f1a

--- tests/pytests.py
+++ tests/pytests.py
@@ -3,10 +3,14 @@ import unittest
 
 from tap import TAPTestRunner
 
+
 if __name__ == '__main__':
     tests_dir = os.path.dirname(os.path.abspath(__file__))
+    case_filter = os.environ.get('PY_RUN_CASE', '')
+    pattern = 'test_*{0}*.py'.format(case_filter)
+
     loader = unittest.TestLoader()
-    tests = loader.discover(tests_dir)
+    tests = loader.discover(tests_dir, pattern=pattern)
 
     runner = TAPTestRunner()
     runner.set_stream(True)
