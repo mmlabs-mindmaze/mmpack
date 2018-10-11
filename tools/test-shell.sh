71abcf1c3fa8673c1ca1fb46d6be2baf1436d1c2
Gabriel Ganne <gabriel.ganne@mindmaze.ch>
2018-10-11 09:06:22 +0200

    fix test-shell tool
    we cannot update a shell variable incrementally within the cat
    
    Change-Id: I854888265203264d100cade86532a1e2695eafd2

--- tools/test-shell.sh
+++ tools/test-shell.sh
@@ -8,26 +8,24 @@ cd $(dirname $0)/../build
 REPOSITORY=${1:-"http://mindmaze-srv-fr-01"}
 MMPACK_PREFIX=$(mktemp -d)
 
+# source mmpack python virtualenv
+testdir=$(pwd)/venv
+python_minor=$(python3 -c 'import sys; print(sys.version_info.minor)')
+python_testdir="${testdir}/lib/python3.${python_minor}"
+
 {
 cat << EOF
 # generate random prefix name
 export MMPACK_PREFIX=${MMPACK_PREFIX}
 
 # system environment execution
-export PATH="${MMPACK_PREFIX}/bin:${PATH}"
+export PATH="${testdir}/bin:${MMPACK_PREFIX}/bin:${PATH}"
 export LD_LIBRARY_PATH="${MMPACK_PREFIX}/lib:${LD_LIBRARY_PATH}"
 export LIBRARY_PATH="${MMPACK_PREFIX}/lib:${LIBRARY_PATH}"
 export CPATH="${MMPACK_PREFIX}/include:${CPATH}"
 
-# source mmpack python virtualenv
-testdir=$(pwd)/venv
-python_minor=$(python3 -c 'import sys; print(sys.version_info.minor)')
-python_testdir="${testdir}/lib/python3.${python_minor}"
-
 export VIRTUAL_ENV=${testdir}
 export PYTHONPATH="${python_testdir}:$python_testdir}/site-packages:${PYTHONPATH}"
-export PATH="${testdir}/bin:${PATH}"
-export LD_LIBRARY_PATH="${testdir}/lib:${LD_LIBRARY_PATH}"
 
 # create mmpack prefix
 mmpack mkprefix "${REPOSITORY}"
