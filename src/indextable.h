9c377e8c2b00a57b7be3c4953a591e66bb68ebca
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2018-10-18 15:38:14 +0200

    Add struct strset to handle set of unique strings
    This is provided as a thin helper layer on top of indextable.
    
    Change-Id: I54c5bbcd8e6cc84bae1ef7dfe762d6857918df87

--- src/indextable.h
+++ src/indextable.h
@@ -65,4 +65,115 @@ struct it_entry* indextable_lookup(const struct indextable* table, const mmstr* 
 struct it_entry* it_iter_first(struct it_iterator* iter, struct indextable const * table);
 struct it_entry* it_iter_next(struct it_iterator* iter);
 
+
+/**************************************************************************
+ *                                                                        *
+ *                          table of unique strings                       *
+ *                                                                        *
+ **************************************************************************/
+
+enum strset_mgmt {
+	STRSET_FOREIGN_STRINGS,
+	STRSET_HANDLE_STRINGS_MEM,
+};
+
+struct strset {
+	struct indextable idx;
+	enum strset_mgmt mem_handling;
+};
+
+struct strset_iterator {
+	struct it_iterator idx_iter;
+};
+
+
+static inline
+int strset_init(struct strset* set, enum strset_mgmt mem_handling)
+{
+	set->mem_handling = mem_handling;
+
+	// Use an non default initial size of the indextable because the
+	// the default indextable size (512) is way too big for the typical
+	// use of struct strset which is meant to list system dependencies
+	// (or maybe file list). A handful number of element should suffice
+	// in most cases.
+	return indextable_init(&set->idx, 10, -1);
+}
+
+
+static inline
+void strset_deinit(struct strset* set)
+{
+	struct it_entry* entry;
+	struct it_iterator iter;
+
+	if (set->mem_handling == STRSET_HANDLE_STRINGS_MEM) {
+		entry = it_iter_first(&iter, &set->idx);
+		while (entry) {
+			mmstr_free(entry->value);
+			entry = it_iter_next(&iter);
+		}
+	}
+	indextable_deinit(&set->idx);
+}
+
+
+static inline
+int strset_add(struct strset* set, const mmstr* str)
+{
+	struct it_entry* entry;
+	mmstr* key;
+
+	entry = indextable_lookup_create(&set->idx, str);
+	if (!entry)
+		return -1;
+
+	if (entry->value)
+		return 0;
+
+	// Copy string if the set must manage the string memory
+	if (set->mem_handling == STRSET_HANDLE_STRINGS_MEM)
+		key = mmstrdup(str);
+	else
+		key = (mmstr*)str;
+
+	entry->key = key;
+	entry->value = key;
+	return 0;
+}
+
+
+static inline
+int strset_contains(const struct strset* set, const mmstr* str)
+{
+	return (indextable_lookup(&set->idx, str) != NULL);
+}
+
+
+static inline
+mmstr* strset_iter_first(struct strset_iterator* iter, const struct strset* set)
+{
+	struct it_entry* entry;
+
+	entry = it_iter_first(&iter->idx_iter, &set->idx);
+	if (!entry)
+		return NULL;
+
+	return entry->value;
+}
+
+
+static inline
+mmstr* strset_iter_next(struct strset_iterator* iter)
+{
+	struct it_entry* entry;
+
+	entry = it_iter_next(&iter->idx_iter);
+	if (!entry)
+		return NULL;
+
+	return entry->value;
+}
+
+
 #endif
