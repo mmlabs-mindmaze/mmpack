19ea7db369860ab377180861002e0088102ce675
Gabriel Ganne <gabriel.ganne@mindmaze.ch>
2019-01-08 11:20:06 +0100

    mmpack-build: add cleaning helper
    Change-Id: I0774f30a6f62fe865a0bb6d294512eb55f5374b9

---
+++ src/mmpack_clean.py
@@ -0,0 +1,25 @@
+# @mindmaze_header@
+'''
+Small helper used to ensure a clean state of mmpack-build work folders
+'''
+
+import sys
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+from workspace import Workspace
+
+
+if __name__ == '__main__':
+    # pylint: disable=invalid-name
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=RawDescriptionHelpFormatter)
+    parser.add_argument('--wipe',
+                        action='store_true',
+                        help='wipe all files including generated packages')
+
+    options = parser.parse_args(sys.argv[1:])
+
+    if options.wipe:
+        Workspace().wipe()
+    else:
+        Workspace().clean()
