92effca2a4d187598ca66f6aad8f7347426d967c
Gabriel Ganne <gabriel.ganne@mindmaze.ch>
2019-01-09 10:14:23 +0100

    add mmpack-check-sysdep
    Command is a wrapper around the check_sysdeps_installed() function.
    
    Change-Id: Ice0ccc49b7246083480010727d5bc78d6a705423

---
+++ src/mmpack-check-sysdep.c
@@ -0,0 +1,29 @@
+/*
+ * @mindmaze_header@
+ */
+
+#if defined (HAVE_CONFIG_H)
+# include <config.h>
+#endif
+
+#include "indextable.h"
+#include "sysdeps.h"
+
+int main(int argc, char const ** argv)
+{
+	int i, rv;
+	struct strset sysdeps;
+
+	if (argc < 2) /* missing package argument */
+		return -1;
+
+	/* convert argument to strset struct */
+	strset_init(&sysdeps, STRSET_FOREIGN_STRINGS);
+	for (i = 1 ; i < argc ; i++)
+		strset_add(&sysdeps, argv[i]);
+
+	rv = check_sysdeps_installed(&sysdeps);
+	strset_deinit(&sysdeps);
+
+	return rv;
+}
