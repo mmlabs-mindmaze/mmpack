d006dc036d11c14744664b0903ca7546fbba53e0
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2019-01-29 20:31:42 +0100

    Add struct buffer
    This provides the facilities of a reallocatable buffer. This can be used
    among other things to implement a stack buffer.
    
    Change-Id: Ib2e1964927d946c55525f61bb57b8c936e70a0e7

--- src/mmpack/utils.c
+++ src/mmpack/utils.c
@@ -591,3 +591,116 @@ int strlist_add(struct strlist* list, const char* str)
 }
 
 
+/**************************************************************************
+ *                                                                        *
+ *                               buffer                                   *
+ *                                                                        *
+ **************************************************************************/
+
+LOCAL_SYMBOL
+void buffer_init(struct buffer* buf)
+{
+	*buf = (struct buffer) {0};
+}
+
+
+LOCAL_SYMBOL
+void buffer_deinit(struct buffer* buf)
+{
+	free(buf->base);
+	*buf = (struct buffer) {0};
+}
+
+
+/**
+ * buffer_reserve_data() - ensure data buffer can welcome a new amount
+ * @buff:       struct buffer to modify
+ * @need_size:  data size to accomodate in addition to the current size
+ *
+ * Return: pointer to data allocated next to content previously added to
+ * @buff. You are guaranteed that up to @need_size can be written to the
+ * returned data.
+ */
+LOCAL_SYMBOL
+void* buffer_reserve_data(struct buffer* buff, size_t need_size)
+{
+	if (buff->size + need_size > buff->max_size) {
+		buff->max_size = next_pow2_u64(buff->size + need_size);
+		buff->base = mm_realloc(buff->base, buff->max_size);
+	}
+
+	return (char*)buff->base + buff->size;
+}
+
+
+/**
+ * buffer_inc_size() - increase memory use in the buffer
+ * @buff:       struct buffer to modify
+ * @size:       number of byte to add to @buff->size
+ *
+ * Return: the new size used by @buff.
+ */
+LOCAL_SYMBOL
+size_t buffer_inc_size(struct buffer* buff, size_t size)
+{
+	buff->size += size;
+	return buff->size;
+}
+
+
+/**
+ * buffer_dec_size() - reduce size used of a buffer
+ * @buff:       struct buffer to modify
+ * @size:       number of byte to reduce
+ *
+ * This function is the exact opposite of buffer_inc_size(). It reduces the
+ * size "used" by the buffer by @size number of bytes. The memory
+ * corresponding to the size reduction memory is still allocated by @buff
+ * and can be accessed until overwritten explicitely.
+ *
+ * Return: the pointer to new top of buffer.
+ */
+LOCAL_SYMBOL
+void* buffer_dec_size(struct buffer* buf, size_t sz)
+{
+	buf->size -= sz;
+	return (char*)buf->base + buf->size;
+}
+
+
+LOCAL_SYMBOL
+void buffer_push(struct buffer* buf, const void* data, size_t sz)
+{
+	memcpy(buffer_reserve_data(buf, sz), data, sz);
+	buffer_inc_size(buf, sz);
+}
+
+
+LOCAL_SYMBOL
+void buffer_pop(struct buffer* buf, void* data, size_t sz)
+{
+	memcpy(data, buffer_dec_size(buf, sz), sz);
+}
+
+
+/**
+ * buffer_take_data_ownership() - steal ownership of the internal data
+ * @buff:       struct buffer to modify
+ *
+ * This function steal ownership of the internal data buffer of @buff.
+ * After this call, @buff will not touch this buffer anymore, later call to
+ * buffer_*() will operate on a new internal data buffer.
+ *
+ * Return: the pointer to the internal buffer. Call free() on it once you
+ * are done with it.
+ */
+LOCAL_SYMBOL
+void* buffer_take_data_ownership(struct buffer* buff)
+{
+	void* data = buff->base;
+
+	buffer_init(buff);
+
+	return data;
+}
+
--- src/mmpack/utils.h
+++ src/mmpack/utils.h
@@ -82,4 +82,26 @@ void strlist_deinit(struct strlist* list);
 int strlist_add(struct strlist* list, const char* str);
 
 
+
+/**************************************************************************
+ *                                                                        *
+ *                               buffer                                   *
+ *                                                                        *
+ **************************************************************************/
+struct buffer {
+	void* base;
+	size_t size;
+	size_t max_size;
+};
+
+
+void buffer_init(struct buffer* buf);
+void buffer_deinit(struct buffer* buf);
+void* buffer_reserve_data(struct buffer* buf, size_t sz);
+size_t buffer_inc_size(struct buffer* buf, size_t sz);
+void* buffer_dec_size(struct buffer* buf, size_t sz);
+void buffer_push(struct buffer* buf, const void* data, size_t sz);
+void buffer_pop(struct buffer* buf, void* data, size_t sz);
+void* buffer_take_data_ownership(struct buffer* buf);
+
 #endif /* UTILS_H */
