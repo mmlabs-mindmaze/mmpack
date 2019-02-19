aa381d00b545165b8e6f771b0a04587ccde15243
Gabriel Ganne <gabriel.ganne@mindmaze.ch>
2019-02-19 10:24:31 +0100

    mmpack-upgrade: only work on selected packages
    Allow to upgrade only given packages. Falls back to upgrade all possible
    packages if none given, as was the behavior before this commit.
    
    If a package, though explicit, is already installed at the latest
    available version, then return a success value (with a message saying
    that nothing was done). This allows for a more permissive usage of the
    function.
    
    Closes: MMLAB-110
    Change-Id: I7b2091b6fdd822d9fcc56f1b2295e7d06cfebd06

--- src/mmpack/mmpack-upgrade.c
+++ src/mmpack/mmpack-upgrade.c
@@ -6,6 +6,7 @@
 # include <config.h>
 #endif
 
+#include <mmargparse.h>
 #include <stdio.h>
 
 #include "action-solver.h"
@@ -15,8 +16,21 @@
 #include "pkg-fs-utils.h"
 
 
+static int is_yes_assumed = 0;
+
+static char upgrade_doc[] =
+	"\"mmpack upgrade\" upgrades given packages, or all possible packages "
+	"if no package is given";
+
+
+static const struct mmarg_opt cmdline_optv[] = {
+	{"y|assume-yes", MMOPT_NOVAL|MMOPT_INT, "1", {.iptr = &is_yes_assumed},
+	 "assume \"yes\" as answer to all prompts and run non-interactively"},
+};
+
+
 static
-struct pkg_request* get_upgradable_reqlist(struct mmpack_ctx* ctx)
+struct pkg_request* get_full_upgradable_reqlist(struct mmpack_ctx* ctx)
 {
 	STATIC_CONST_MMSTR(any_version, "any")
 	struct pkg_request *req, *reqlist;
@@ -47,6 +61,42 @@ struct pkg_request* get_upgradable_reqlist(struct mmpack_ctx* ctx)
 
 
 static
+struct pkg_request* get_upgradable_reqlist(struct mmpack_ctx* ctx, int nreq,
+                                           char const ** req_args)
+{
+	STATIC_CONST_MMSTR(any_version, "any")
+	int i;
+	struct pkg_request *req, *reqlist;
+	mmstr * pkg_name;
+	const struct mmpkg* pkg;
+	const struct mmpkg* latest;
+
+	reqlist = NULL;
+
+	for (i = 0 ; i < nreq ; i++) {
+		pkg_name = mmstr_malloc_from_cstr(req_args[i]);
+		pkg = install_state_get_pkg(&ctx->installed, pkg_name);
+		mmstr_free(pkg_name);
+		latest = binindex_get_latest_pkg(&ctx->binindex, pkg->name, any_version);
+
+		if (pkg_version_compare(pkg->version, latest->version) >= 0) {
+			printf("Package \"%s\" is already at its latest possible version (%s).\n",
+					pkg->name, pkg->version);
+			continue;
+		}
+
+		req = mm_malloc(sizeof(*req));
+		req->name = pkg->name;
+		req->version = NULL;
+		req->next = reqlist;
+		reqlist = req;
+	}
+
+	return reqlist;
+}
+
+
+static
 void clean_reqlist(struct pkg_request* reqlist)
 {
 	struct pkg_request* next;
@@ -56,6 +106,33 @@ void clean_reqlist(struct pkg_request* reqlist)
 		free(reqlist);
 		reqlist = next;
 	}
+}
+
+
+static
+int mmpack_upgrade_reqlist(struct mmpack_ctx * ctx, struct pkg_request* reqlist)
+{
+
+	struct action_stack* act_stack = NULL;
+	int rv = -1;
+
+	// Determine the stack of actions to perform
+	act_stack = mmpkg_get_upgrade_list(ctx, reqlist);
+	if (!act_stack)
+		goto exit;
+
+	if (!is_yes_assumed) {
+		rv = confirm_action_stack_if_needed(0, act_stack);
+		if (rv != 0)
+			goto exit;
+	}
+
+	rv = apply_action_stack(ctx, act_stack);
+
+exit:
+	mmpack_action_stack_destroy(act_stack);
+	return rv;
+
 }
 
 
@@ -74,40 +151,35 @@ void clean_reqlist(struct pkg_request* reqlist)
 LOCAL_SYMBOL
 int mmpack_upgrade(struct mmpack_ctx * ctx, int argc, char const ** argv)
 {
-	struct action_stack* act_stack = NULL;
-	int rv = -1;
+	int nreq, arg_index, rv;
+	const char** req_args;
 	struct pkg_request* reqlist;
+	struct mmarg_parser parser = {
+		.doc = upgrade_doc,
+		.args_doc = UPGRADE_SYNOPSIS,
+		.optv = cmdline_optv,
+		.num_opt = MM_NELEM(cmdline_optv),
+		.execname = "mmpack",
+	};
 
-	if (argc == 2
-	    && (STR_EQUAL(argv[1], strlen(argv[1]), "--help")
-	        || STR_EQUAL(argv[1], strlen(argv[1]), "-h"))) {
-		fprintf(stderr, "Usage:\n\tmmpack "UPGRADE_SYNOPSIS"\n");
-		return 0;
-	}
+	arg_index = mmarg_parse(&parser, argc, (char**)argv);
+	nreq = argc - arg_index;
+	req_args = argv + arg_index;
 
-	// Load prefix configuration and caches
+	/* Load prefix configuration and caches */
 	if (mmpack_ctx_use_prefix(ctx, 0))
 		return -1;
 
-	reqlist = get_upgradable_reqlist(ctx);
-	if (!reqlist) {
-		printf("Nothing to do.\n");
-		return 0;
+	if (nreq == 0)
+		reqlist = get_upgradable_reqlist(ctx, nreq, req_args);
+	else
+		reqlist = get_full_upgradable_reqlist(ctx);
+
+	rv = 0;
+	if (reqlist != NULL) {
+		rv = mmpack_upgrade_reqlist(ctx, reqlist);
+		clean_reqlist(reqlist);
 	}
 
-	// Determine the stack of actions to perform
-	act_stack = mmpkg_get_upgrade_list(ctx, reqlist);
-	if (!act_stack)
-		goto exit;
-
-	rv = confirm_action_stack_if_needed(0, act_stack);
-	if (rv != 0)
-		goto exit;
-
-	rv = apply_action_stack(ctx, act_stack);
-
-exit:
-	mmpack_action_stack_destroy(act_stack);
-	clean_reqlist(reqlist);
 	return rv;
 }
--- src/mmpack/mmpack-upgrade.h
+++ src/mmpack/mmpack-upgrade.h
@@ -7,7 +7,7 @@
 
 #include "context.h"
 
-#define UPGRADE_SYNOPSIS "upgrade"
+#define UPGRADE_SYNOPSIS "upgrade [upgrade-opts] [<pkg> [...]]"
 
 int mmpack_upgrade(struct mmpack_ctx * ctx, int argc, char const ** argv);
 
