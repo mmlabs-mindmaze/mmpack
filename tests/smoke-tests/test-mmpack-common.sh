26b0c64cd8641fef654f4de354018b1e60ede653
Gabriel Ganne <gabriel.ganne@mindmaze.ch>
2019-10-26 09:18:37 +0200

    tests - factorize mmpack smoke tests code
    Change-Id: I878ba691023515c10a2df22e48370dcfa961d1c5

---
+++ tests/smoke-tests/test-mmpack-common.sh
@@ -0,0 +1,43 @@
+
+prepare_env()
+{
+	# deal with windows environment
+	if [ -n "$(which cygpath)" ] ; then
+		SRCDIR=$(cygpath -u $SRCDIR)
+		BUILDDIR=$(cygpath -u $BUILDDIR)
+		PREFIX=$(cygpath -u $PREFIX)
+		_MMPACK_TEST_PREFIX=$(cygpath -u $_MMPACK_TEST_PREFIX)
+	fi
+
+	# initialisation of variables
+	PACKAGE=$BUILDDIR/packages
+	PREFIX_TEST=$BUILDDIR/prefix
+	CREATE=$BUILDDIR/tmp
+	TRASH=$BUILDDIR/trash
+	TEST_SRCDIR=$SRCDIR/tests/smoke-tests
+
+	# set the environment variables for this test
+	name="$(basename $0)"
+	PREFIX_TEST+=_${name%.*}
+	export PACKAGE+=_${name%.*}
+	export CREATE+=_${name%.*}
+	export TRASH+=_${name%.*}
+
+	# set the environment properly
+	PATH=$_MMPACK_TEST_PREFIX$PREFIX/bin:$PATH
+	export MMPACK_PREFIX=$PREFIX_TEST
+}
+
+# clean the files and repositories necessary for the tests
+cleanup()
+{
+	rm -rf $PACKAGE
+	rm -rf $PREFIX_TEST
+}
+
+create-test-pkg()
+{
+	# creation of a package hello.mpk
+	mkdir $PACKAGE
+	$TEST_SRCDIR/create_package_hello.sh
+}
--- tests/smoke-tests/test_mmpack_check-integrity.sh
+++ tests/smoke-tests/test_mmpack_check-integrity.sh
@@ -2,50 +2,15 @@
 
 set -ex
 
-# deal with windows environment
-if [ -n "$(which cygpath)" ] ; then
-	SRCDIR=$(cygpath -u $SRCDIR)
-	BUILDDIR=$(cygpath -u $BUILDDIR)
-	PREFIX=$(cygpath -u $PREFIX)
-	_MMPACK_TEST_PREFIX=$(cygpath -u $_MMPACK_TEST_PREFIX)
-fi
+. $(dirname $0)/test-mmpack-common.sh
+prepare_env
 
-# initialisation of variables
-PACKAGE=$BUILDDIR/packages
-PREFIX_TEST=$BUILDDIR/prefix
-CREATE=$BUILDDIR/tmp
-TRASH=$BUILDDIR/trash
-TEST_SRCDIR=$SRCDIR/tests/smoke-tests
-
-# clean the files and repositories necessary for the tests
-cleanup()
-{
-	rm -rf $PACKAGE
-	rm -rf $PREFIX_TEST
-}
 trap cleanup EXIT
-
 cleanup
 
-# set the environment variables for this test
-name="$(basename $0)"
-PREFIX_TEST+=_${name%.*}
-PACKAGE+=_${name%.*}
-CREATE+=_${name%.*}
-TRASH+=_${name%.*}
-
-# set the environment properly
-PATH=$_MMPACK_TEST_PREFIX$PREFIX/bin:$PATH
-export MMPACK_PREFIX=$PREFIX_TEST
-
-# creation of a package hello.mpk
-export CREATE
-export PACKAGE
-export TRASH
-mkdir $PACKAGE
-$TEST_SRCDIR/create_package_hello.sh
-
+create-test-pkg
 env
+
 
 mmpack mkprefix $PREFIX_TEST
 mmpack install $PACKAGE/hello*.mpk
