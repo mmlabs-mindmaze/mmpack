e9d39e63f18b8a969f25415a2b00e05773dd69ba
Marjorie Bournat <marjorie.bournat@mindmaze.com>
2019-11-04 18:00:06 +0100

    mmpack: fix mmpack repo
    Before, when adding a repository, no binindex file associated was
    created. Then, no mmpack command was usable, except if mmpack update
    was executed. Similarly, before, when removing a repository the
    bindindex file was not destroyed. And also, before, when renaming a
    repository, the binindex file was not renamed. Now, these "bugs"
    are fixed.
    
    Change-Id: I2e81b4986b33dc0cebe3670be817b45835c86b75

--- src/mmpack/mmpack-repo.c
+++ src/mmpack/mmpack-repo.c
@@ -10,6 +10,7 @@
 #include <mmerrno.h>
 #include <mmlib.h>
 #include <mmsysio.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "cmdline.h"
@@ -17,7 +18,12 @@
 #include "context.h"
 #include "mmpack-repo.h"
 #include "mmstring.h"
+#include "package-utils.h"
+#include "settings.h"
 #include "utils.h"
+
+
+STATIC_CONST_MMSTR(repo_relpath, REPO_INDEX_RELPATH);
 
 
 static
@@ -30,6 +36,11 @@ int repo_add(struct mmpack_ctx* ctx, int argc, char const ** argv)
 
 	if (repolist_add(&ctx->settings.repo_list, argv[0], argv[1]) != 0)
 		return -1;
+
+	if (create_empty_binindex_file(ctx->prefix, argv[0]) == -1) {
+		info("binindex file was not created\n");
+		return -1;
+	}
 
 	return settings_serialize(ctx->prefix, &ctx->settings, 1);
 }
@@ -56,6 +67,30 @@ int repo_list(struct mmpack_ctx* ctx, int argc, char const ** argv)
 
 
 static
+int remove_binindex_file(const mmstr* prefix, char const * name)
+{
+	mmstr * avllist_relpath;
+	int rv = 0;
+	int len;
+
+	len = mmstrlen(prefix) + mmstrlen(repo_relpath) + strlen(name) + 2;
+	avllist_relpath = mmstr_malloc(len);
+
+	mmstr_join_path(avllist_relpath, prefix, repo_relpath);
+
+	// Append the name of the repo
+	mmstrcat_cstr(mmstrcat_cstr(avllist_relpath, "."), name);
+
+	if (mm_unlink(avllist_relpath) != 0) {
+		rv = -1;
+	}
+
+	mmstr_free(avllist_relpath);
+	return rv;
+}
+
+
+static
 int repo_remove(struct mmpack_ctx* ctx, int argc, char const ** argv)
 {
 	if (argc != 1) {
@@ -68,7 +103,42 @@ int repo_remove(struct mmpack_ctx* ctx, int argc, char const ** argv)
 		return -1;
 	}
 
+	remove_binindex_file(ctx->prefix, argv[0]);
+
 	return settings_serialize(ctx->prefix, &ctx->settings, 1);
+}
+
+
+static
+int rename_binindex_file(const mmstr* prefix, char const * old_name,
+                         char const * new_name)
+{
+	mmstr * old, * new;
+	int old_len, new_len;
+	int rv = 0;
+
+	// create the path of the old and new names
+	old_len = mmstrlen(prefix) + mmstrlen(repo_relpath) +
+	          strlen(old_name) + 2;
+	new_len = mmstrlen(prefix) + mmstrlen(repo_relpath) +
+	          strlen(new_name) + 2;
+
+	old = mmstr_malloca(old_len);
+	new = mmstr_malloca(new_len);
+
+	mmstr_join_path(old, prefix, repo_relpath);
+	mmstrcat_cstr(mmstrcat_cstr(old, "."), old_name);
+
+	mmstr_join_path(new, prefix, repo_relpath);
+	mmstrcat_cstr(mmstrcat_cstr(new, "."), new_name);
+
+	if (rename(old, new) != 0) {
+		rv = -1;
+	}
+
+	mmstr_freea(old);
+	mmstr_freea(new);
+	return rv;
 }
 
 
@@ -84,12 +154,19 @@ int repo_rename(struct mmpack_ctx* ctx, int argc, char const ** argv)
 	int name_len = strlen(name);
 	struct repolist_elt * elt = ctx->settings.repo_list.head;
 
+	// check that no repository possesses already this name
+	if (repolist_lookup(&ctx->settings.repo_list, argv[1])) {
+		error("repository \"%s\" already exists\n", argv[1]);
+		return -1;
+	}
+
 	while (elt != NULL) {
 		if (name_len == mmstrlen(elt->name)
 		    && strncmp(elt->name, name, name_len) == 0) {
 
 			mmstr_free(elt->name);
 			elt->name = mmstr_malloc_from_cstr(argv[1]);
+			rename_binindex_file(ctx->prefix, argv[0], argv[1]);
 			return settings_serialize(ctx->prefix,
 			                          &ctx->settings, 1);
 		}
