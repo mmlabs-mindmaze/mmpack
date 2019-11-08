563fb4ac4886ed2ba9e316371caac2437c057713
Gabriel Ganne <gabriel.ganne@mindmaze.com>
2019-11-08 11:28:18 +0100

    test - add test for "mmpack check-broken"
    increase the check-integrity test instead of creating a new one
    
    Change-Id: I5f7665d649f0174147a2fbb3ed89b24c3798f8a6

--- tests/smoke-tests/test_mmpack_check-integrity.sh
+++ tests/smoke-tests/test_mmpack_check-integrity.sh
@@ -12,3 +12,30 @@ mmpack mkprefix $PREFIX_TEST
 mmpack install $REPO/hello-data_1.0.0*.mpk
 
 mmpack check-integrity
+
+# compromized integrity detection works
+# => try to fix
+# this needs a repository containing the good package
+cleanup
+
+mmpack mkprefix --url=$REPO_URL $PREFIX_TEST
+mmpack update
+mmpack install -y hello
+
+$PREFIX_TEST/bin/hello-world
+
+echo "" > $PREFIX_TEST/bin/hello-world
+
+mmpack check-integrity && false || echo "Fail as expected"
+
+mmpack fix-broken hello
+
+# should be good now
+mmpack check-integrity
+
+# should not do anything
+tree_before=$(find $PREFIX_TEST -type f | sort | sha1sum)
+mmpack fix-broken
+tree_after=$(find $PREFIX_TEST -type f | sort | sha1sum)
+
+[ "$tree_before" == "$tree_after" ]
