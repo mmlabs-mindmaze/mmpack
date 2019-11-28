d3ce3f56c73ae742c9303e70b4bd06c7c118a56d
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2019-11-28 09:27:50 +0100

    mmpack-build: add hook_locales
    Add a new hook for mmpack-build to package the internationalization
    files. This hook will package by default those files in a dedicated
    package called <srcname>-locales
    
    Change-Id: I5e32e0792db1878ddbba291526db147f90ee56fe

---
+++ src/mmpack-build/hook_locales.py
@@ -0,0 +1,46 @@
+# @mindmaze_header@
+"""
+plugin to package internationalization files
+
+TODO: analyze the strings used in other cobuilded packages to generate the
+right relation between the -locales package and those that use the translated
+strings
+"""
+
+import re
+from typing import Dict, Set
+
+from . base_hook import BaseHook
+
+
+class MMPackBuildHook(BaseHook):
+    """
+    Hook tracking internationalization files
+    """
+
+    def get_dispatch(self, install_files: Set[str]) -> Dict[str, Set[str]]:
+        """
+        Unless specified otherwise in mmpack specs, all internationalization
+        files are packaged together in a dedicated package.
+
+        The rationale for this policy is that:
+          1 - translations are (usually) not manadatory for program to function
+          2 - translations are often shared for all libraries and executable
+              provided by the same project.
+          3 - if locales are used exclusivily by a specific lib and executable,
+              nothing guarantees that this will be the case in future.
+          4 - if a shared lib package contains its translation files, this will
+              prevent smooth transition in case of ABI break of a the lib: the
+              abi break will result in two shared lib packages containing the
+              same files, which will prevent to coinstall those two packages
+              (which is the guarantee for a smooth transition).
+        """
+        pkgs = dict()
+
+        locales_re = re.compile(r'share/locale/.*')
+        locales_files = {f for f in install_files if locales_re.match(f)}
+
+        if locales_files:
+            pkgs[self._srcname + '-locales'] = locales_files
+
+        return pkgs
