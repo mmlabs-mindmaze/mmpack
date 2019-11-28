68c0586ebd30d08a916df719c9a30a392b5cf083
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2019-11-28 13:51:35 +0100

    mmpack-build: refactor source_tarball.py
    Move the source package dir creation and upstream retrieval functions
    into in private methods of SourceTarball class
    
    Change-Id: I94ab0b5d652b467a32793918351011c7a36e9314

--- src/mmpack-build/source_tarball.py
+++ src/mmpack-build/source_tarball.py
@@ -46,172 +46,12 @@ def _git_clone(url: str, clonedir: str, tag: str = None,
     shell(git_clone_sh_cmd)
 
 
-###########################################################################
-#
-#             Create mmpack source dir package
-#
-###########################################################################
-def _create_srcdir_from_git(builddir: str, url: str,
-                            tag: str, **kwargs) -> str:
-    """
-    Create a source package folder from git clone
-
-    Args:
-        builddir: folder where sources must be cloned
-        url: url of the git repository
-        tag: the name of the commit/tag/branch to checkout (may be None)
-        **kwargs: supported keyword arguments are following
-            git_ssh_cmd: ssh cmd to use when cloning git repo through ssh
-
-    Returns:
-        tag that have been checked out.
-    """
-    _git_clone(url, builddir, tag, kwargs.get('git_ssh_cmd'))
-    git_dir = builddir + '/.git'
-
-    # Get tag name if not set yet (use current branch)
-    if not tag:
-        tag = _git_subcmd('rev-parse --abbrev-ref HEAD', git_dir)
-
-    shutil.rmtree(git_dir, ignore_errors=True)
-
-    return tag
-
-
-def _create_srcdir_from_tar(builddir: str, filename: str,
-                            tag: str, **kwargs) -> str:
-    """
-    Create a source package folder from tarball, not necessarily named
-    following mmpack src tarball convention.
-
-    Args:
-        builddir: folder where sources must be extracted
-        filename: filename of the tarball
-        tag: the name of the tag (only transmitted in later stage), can be None
-        **kwargs: keyword arguments (ignored, used on other method)
-
-    Returns:
-        `tag` if not None, 'from_par' otherwise
-    """
-    tar = tarfile.open(filename, 'r:*')
-    tar.extractall(path=builddir)
-
-    return tag if tag else 'from_tar'
-
-
-def _create_srcdir(method: str, builddir: str,
-                   path_url: str, tag: str, **kwargs) -> str:
-    """
-    Fetch source using specified method and extract it to src dir
-
-    Args:
-        method: 'tar', 'git' or srcpkg
-        builddir: folder where sources must be extracted
-        path_url: path or url to the mmpack sources
-        tag: the name of the commit/tag/branch to checkout (may be None)
-        **kwargs: supported keyword arguments passed to specific method
-    """
-    method_mapping = {
-        'git': _create_srcdir_from_git,
-        'tar': _create_srcdir_from_tar,
-        'srcpkg': _create_srcdir_from_tar,
-    }
-
-    create_srcdir_fn = method_mapping.get(method)
-    if not create_srcdir_fn:
-        raise ValueError("Invalid method " + method)
-
-    return create_srcdir_fn(builddir, path_url, tag, **kwargs)
-
-
-###########################################################################
-#
-#              Fetch upstream sources
-#
-###########################################################################
-def _fetch_upstream_from_git(srcdir: str, specs: Dict[str, str]) -> str:
-    """
-    Fetch upstream sources from git clone
-
-    Args:
-        srcdir: folder where sources must be cloned
-        specs: dict of settings put in source-strap file
-    """
-    _git_clone(specs['url'], srcdir, specs.get('branch'))
-    shutil.rmtree(srcdir + '/.git')
-
-
-def _fetch_upstream_from_tar(srcdir: str, specs: Dict[str, str]) -> str:
-    """
-    Fetch upstream sources from remote tar
-
-    Args:
-        srcdir: folder where sources must be extracted
-        specs: dict of settings put in source-strap file
-    """
-    url = specs['url']
-    filename = os.path.basename(url)
-    downloaded_file = os.path.normpath(os.path.join(srcdir, '..', filename))
-
-    # Download remote tar
-    iprint('Downloading {}...'.format(url))
-    request = urllib3.PoolManager().request('GET', url)
-    with open(downloaded_file, 'wb') as outfile:
-        outfile.write(request.data)
-    iprint('Done')
-
-    # Verify sha256 is correct if supplied in specs
-    if 'sha256' in specs and specs['sha256'] != sha256sum(downloaded_file):
-        raise Assert("Downloaded file does not match expected sha256")
-
-    # Extract downloaded_file in srcdir
-    with tarfile.open(downloaded_file, 'r:*') as tar:
-        tar.extractall(path=srcdir)
-
-    os.remove(downloaded_file)
-
-
-def _fetch_upstream(srcdir: str, specs: Dict[str, str]):
-    """
-    Fetch upstream sources using specified method and extract it to src dir
-
-    Args:
-        srcdir: folder where sources must be extracted
-        specs: dict of settings put in source-strap file
-    """
-    method_mapping = {
-        'git': _fetch_upstream_from_git,
-        'tar': _fetch_upstream_from_tar,
-    }
-
-    # check that mandatory keys are present in sources-strap file
-    missings_keys = {'method', 'url'}.difference(specs)
-    if missings_keys:
-        raise Assert('missing mandatory keys in source-strap: {}'
-                     .format(', '.join(missings_keys)))
-
-    # Select proper _fetch_upstream_* function according to method entry
-    method = specs['method']
-    fetch_upstream_fn = method_mapping.get(method)
-    if not fetch_upstream_fn:
-        raise Assert("Invalid method " + method)
-
-    # execute selected method
-    fetch_upstream_fn(srcdir, specs)
-
-
-###########################################################################
-#
-#              Source Tarball class
-#
-###########################################################################
 class SourceTarball:
     """
     Class managing source tarball creation
     """
-
     def __init__(self, method: str, path_url: str, tag: str = None,
-                 outdir: str = None, **kwargs):
+                 outdir: str = Workspace().packages, **kwargs):
         """
         Create a source package from various methods
 
@@ -221,26 +61,22 @@ class SourceTarball:
             tag: the name of the commit/tag/branch to checkout (may be None)
             outdir: folder where to put the generated source package. If None,
                 it will be located in Workspace().packages
-            **kwargs: supported keyword arguments are following
+            **kwargs: supported optional keyword arguments are following
                 git_ssh_cmd: ssh cmd to use when cloning git repo through ssh
         """
         # declare class instance attributes
-        self._srcdir = None
         self.srctar = None
-        self.tag = None
+        self.tag = tag
         self.name = None
-
-        wrk = Workspace()
-        self._srcdir = mkdtemp(dir=wrk.sources)
-        if not outdir:
-            outdir = wrk.packages
+        self._path_url = path_url
+        self._kwargs = kwargs
+        self._srcdir = mkdtemp(dir=Workspace().sources)
 
         # Fetch sources following the specified method and move them to the
         # temporary source build folder
         dprint('extracting sources in the temporary directory: {}'
                .format(self._srcdir))
-        self.tag = _create_srcdir(method, self._srcdir,
-                                  path_url, tag, **kwargs)
+        self._create_srcdir(method)
 
         # extract minimal metadata from package
         try:
@@ -268,6 +104,13 @@ class SourceTarball:
             dprint('Destroying temporary source build dir ' + self._srcdir)
             shutil.rmtree(self._srcdir)
 
+    def _get_unpacked_upstream_dir(self):
+        """
+        Get location when upstream source code is expected to be extracted if
+        fetched. This does not created the folder.
+        """
+        return os.path.join(self._srcdir, 'mmpack/upstream')
+
     def _process_source_strap(self):
         source_strap = os.path.join(self._srcdir, 'mmpack/sources-strap')
         try:
@@ -275,16 +118,7 @@ class SourceTarball:
         except FileNotFoundError:
             return  # There is no source strap, nothing to be done
 
-        upstream_srcdir = os.path.join(self._srcdir, 'mmpack/upstream')
-        _fetch_upstream(upstream_srcdir, specs)
-
-        # Move extracted upstream sources except mmpack packaging
-        for elt in os.listdir(upstream_srcdir):
-            if elt != 'mmpack':
-                shutil.move(os.path.join(upstream_srcdir, elt), self._srcdir)
-
-        # Clean leftover of temporary extracted
-        shutil.rmtree(upstream_srcdir)
+        self._fetch_upstream(specs)
 
     def detach_srcdir(self) -> str:
         """
@@ -315,3 +149,123 @@ class SourceTarball:
 
         self._srcdir = unpackdir
         self.srctar = new_srctar
+
+    def _create_srcdir_from_git(self):
+        """
+        Create a source package folder from git clone
+        """
+        git_ssh_cmd = self._kwargs.get('git_ssh_cmd')
+        _git_clone(self._path_url, self._srcdir, self.tag, git_ssh_cmd)
+        git_dir = self._srcdir + '/.git'
+
+        # Get tag name if not set yet (use current branch)
+        if not self.tag:
+            self.tag = _git_subcmd('rev-parse --abbrev-ref HEAD', git_dir)
+
+        shutil.rmtree(git_dir, ignore_errors=True)
+
+    def _create_srcdir_from_tar(self):
+        """
+        Create a source package folder from tarball, not necessarily named
+        following mmpack src tarball convention.
+        """
+        tar = tarfile.open(self._path_url, 'r:*')
+        tar.extractall(path=self._srcdir)
+
+        # Get tag name if not set yet
+        if not self.tag:
+            self.tag = 'from_tar'
+
+    def _create_srcdir(self, method: str):
+        """
+        Fetch source using specified method and extract it to src dir
+
+        Args:
+            method: 'tar', 'git' or 'srcpkg'
+        """
+        method_mapping = {
+            'git': self._create_srcdir_from_git,
+            'tar': self._create_srcdir_from_tar,
+            'srcpkg': self._create_srcdir_from_tar,
+        }
+
+        create_srcdir_callable = method_mapping.get(method)
+        if not create_srcdir_callable:
+            raise ValueError("Invalid method " + method)
+
+        create_srcdir_callable()
+
+    def _fetch_upstream_from_git(self, specs: Dict[str, str]):
+        """
+        Fetch upstream sources from git clone
+
+        Args:
+            specs: dict of settings put in source-strap file
+        """
+        srcdir = self._get_unpacked_upstream_dir()
+        _git_clone(specs['url'], srcdir, specs.get('branch'))
+        shutil.rmtree(srcdir + '/.git')
+
+    def _fetch_upstream_from_tar(self, specs: Dict[str, str]):
+        """
+        Fetch upstream sources from remote tar
+
+        Args:
+            specs: dict of settings put in source-strap file
+        """
+        url = specs['url']
+        filename = os.path.basename(url)
+        downloaded_file = os.path.join(self._srcdir, 'mmpack', filename)
+
+        # Download remote tar
+        iprint('Downloading {}...'.format(url))
+        request = urllib3.PoolManager().request('GET', url)
+        with open(downloaded_file, 'wb') as outfile:
+            outfile.write(request.data)
+        iprint('Done')
+
+        # Verify sha256 is correct if supplied in specs
+        if 'sha256' in specs and specs['sha256'] != sha256sum(downloaded_file):
+            raise Assert("Downloaded file does not match expected sha256")
+
+        # Extract downloaded_file in upstreamdir
+        with tarfile.open(downloaded_file, 'r:*') as tar:
+            tar.extractall(path=self._get_unpacked_upstream_dir())
+
+        os.remove(downloaded_file)
+
+    def _fetch_upstream(self, specs: Dict[str, str]):
+        """
+        Fetch upstream sources using specified method and extract it to src dir
+
+        Args:
+            specs: dict of settings put in source-strap file
+        """
+        method_mapping = {
+            'git': self._fetch_upstream_from_git,
+            'tar': self._fetch_upstream_from_tar,
+        }
+
+        # check that mandatory keys are present in sources-strap file
+        missings_keys = {'method', 'url'}.difference(specs)
+        if missings_keys:
+            raise Assert('missing mandatory keys in source-strap: {}'
+                         .format(', '.join(missings_keys)))
+
+        # Select proper _fetch_upstream_* function according to method entry
+        method = specs['method']
+        fetch_upstream_callable = method_mapping.get(method)
+        if not fetch_upstream_callable:
+            raise Assert("Invalid method " + method)
+
+        # execute selected method
+        fetch_upstream_callable(specs)
+
+        # Move extracted upstream sources except mmpack packaging
+        upstream_srcdir = self._get_unpacked_upstream_dir()
+        for elt in os.listdir(upstream_srcdir):
+            if elt != 'mmpack':
+                shutil.move(os.path.join(upstream_srcdir, elt), self._srcdir)
+
+        # Clean leftover of temporary extracted
+        shutil.rmtree(upstream_srcdir)
