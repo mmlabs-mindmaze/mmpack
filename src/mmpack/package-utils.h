46e5434c4e520f014ff66be8e660c967fd8ac5d7
Marjorie Bournat <marjorie.bournat@mindmaze.com>
2019-12-24 15:32:54 +0100

    mmpack: use a structure constraints
    Add a structure pkg_parser used when parsing the commandline: it
    is filled with requested packages. This structure is composed of
    a structure of constraints that reprensent the filter that the user
    wants to apply in order to select a specific package. The function
    binindex_lookup takes now in argument this structure of constraints
    to search for the appropriate package.
    
    This change will permit to have eventually an easier code to maintain
    and extend. Indeed, we can add easily new constraints on the package
    searched: it suffices to add a field in the structure of constraints,
    and to filter on this new field in the function binindex_lookup.
    Note that this implementation will avoid the duplication of the code in
    each command that support the same new option (e.g., if we want to
    filter on the repositories, we need to add the option --repo in all the
    commands that wants to apply this filter).
    
    Change-Id: Ib71b568554bf49fbf22b570d49759597a5dcfb48

--- src/mmpack/package-utils.h
+++ src/mmpack/package-utils.h
@@ -34,6 +34,20 @@ typedef enum {
 
 int pkg_version_compare(char const * v1, char const * v2);
 
+
+/**
+ * strcut constraints - structure containing all the possible constraints
+ *                      imposed by the user in the command line.
+ * @version: package version
+ */
+struct constraints {
+	const mmstr * version;
+};
+
+void constraints_init(struct constraints * c);
+void constraints_deinit(struct constraints * c);
+
+
 struct from_repo {
 	mmstr const * filename;
 	mmstr const * sha256;
@@ -159,7 +173,8 @@ void mmpkg_dep_dump(struct mmpkg_dep const * deps, char const * type);
 void mmpkg_dep_save_to_index(struct mmpkg_dep const * dep, FILE* fp, int lvl);
 
 struct mmpkg const* binindex_lookup(struct binindex* binindex,
-                                    mmstr const * name, char const * version);
+                                    mmstr const * name,
+                                    struct constraints const * c);
 int binindex_is_pkg_upgradeable(struct binindex const * binindex,
                                 struct mmpkg const * pkg);
 void binindex_init(struct binindex* binindex);
