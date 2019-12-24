1b56934cd2294413060b95e2a4eefccb5a4d5d8f
Marjorie Bournat <marjorie.bournat@mindmaze.com>
2019-12-24 11:21:21 +0100

    mmpack: add filtering on repo for mmpack install
    It is now possible to select the repository from which we want to
    install a package.
    
    Change-Id: I54d29ccf4244362295be17702c6b431eb6782fcc

--- tests/smoke-tests/test_mmpack_install.sh
+++ tests/smoke-tests/test_mmpack_install.sh
@@ -8,7 +8,20 @@ prepare_env
 trap cleanup EXIT
 cleanup
 
-mmpack mkprefix $PREFIX_TEST
+mmpack mkprefix --url=$REPO_URL $PREFIX_TEST
 mmpack install -y $REPO/hello_1.0.0.mpk $REPO/hello-data_1.0.0.mpk
 
+$PREFIX_TEST/bin/hello-world
+
+mmpack uninstall hello hello-data
+mmpack update
+
+# test filtering on a specific repository
+diff - <(mmpack install --repo=non_existing_repo hello-data | $dos2unix) << EOF
+Repository non_existing_repo not found
+EOF
+
+echo "on est là"
+
+mmpack install -y --repo=repo-0 hello-data hello
 $PREFIX_TEST/bin/hello-world
