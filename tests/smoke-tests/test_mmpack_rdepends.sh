9631fb946c7937845bc49759118ff2100691bc45
Marjorie Bournat <marjorie.bournat@mindmaze.com>
2019-12-24 15:32:54 +0100

    mmpack: add a structure constraints
    Add a structure constraints that contains all the possible fields that
    the user may constraint by the command line.
    
    Change-Id: Ib71b568554bf49fbf22b570d49759597a5dcfb48

--- tests/smoke-tests/test_mmpack_rdepends.sh
+++ tests/smoke-tests/test_mmpack_rdepends.sh
@@ -12,10 +12,10 @@ mmpack mkprefix --name=repo --url=$REPO_URL $PREFIX_TEST
 mmpack update
 
 output="$(mmpack rdepends toto | $dos2unix)"
-expected="No package toto (any version)"
+expected="Package toto not found"
 [ "$output" == "$expected" ]
 
-output="$(mmpack rdepends --repo=repo hello | $dos2unix)"
+output="$(mmpack rdepends hello=fromrepo:repo | $dos2unix)"
 expected="call-hello (1.0.0)"
 [ "$output" == "$expected" ]
 
@@ -23,7 +23,7 @@ output="$(mmpack rdepends call-hello | $dos2unix)"
 expected=""
 [ "$output" == "$expected" ]
 
-output="$(mmpack rdepends --recursive --repo=repo call-hello | $dos2unix)"
+output="$(mmpack rdepends --recursive call-hello=fromrepo:repo | $dos2unix)"
 expected=""
 [ "$output" == "$expected" ]
 
@@ -31,11 +31,11 @@ output="$(mmpack rdepends hello-data | $dos2unix)"
 expected="hello (1.0.0)"
 [ "$output" == "$expected" ]
 
-output="$(mmpack rdepends --repo=repo hello-data=1.0.0 | $dos2unix)"
+output="$(mmpack rdepends hello-data=1.0.0fromrepo:repo | $dos2unix)"
 expected="hello (1.0.0)"
 [ "$output" == "$expected" ]
 
-output="$(mmpack rdepends --recursive --repo=repo hello-data | $dos2unix)"
+output="$(mmpack rdepends --recursive hello-data=fromrepo:repo | $dos2unix)"
 expected="call-hello (1.0.0)
 hello (1.0.0)"
 [ "$output" == "$expected" ]
@@ -45,16 +45,16 @@ expected="call-hello (1.0.0)
 hello (1.0.0)"
 [ "$output" == "$expected" ]
 
-output="$(mmpack rdepends --sumsha 9f68904c9c1761388382afdb7d7b7618101e353b5b93b872b10cc71f0a7c8a34 | $dos2unix)"
+output="$(mmpack rdepends hello-data=sumsha:9f68904c9c1761388382afdb7d7b7618101e353b5b93b872b10cc71f0a7c8a34 | $dos2unix)"
 expected="hello (1.0.0)"
 [ "$output" == "$expected" ]
 
-output="$(mmpack rdepends --sumsha --recursive 8dc8f06e9277b4404661045a43d31dbc0f85bebd5f26e29a801a0efb12d064b6 | $dos2unix)"
+output="$(mmpack rdepends --recursive hello-data=sumsha:8dc8f06e9277b4404661045a43d31dbc0f85bebd5f26e29a801a0efb12d064b6 | $dos2unix)"
 expected="call-hello (1.0.0)
 hello (1.0.0)"
 [ "$output" == "$expected" ]
 
-output="$(mmpack rdepends --sumsha --recursive 0000 | $dos2unix)"
-expected="No package with sumsha: 0000"
+output="$(mmpack rdepends --recursive toto=sumsha:0000 | $dos2unix)"
+expected="Package toto not found"
 [ "$output" == "$expected" ]
 
