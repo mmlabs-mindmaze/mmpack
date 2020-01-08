71ee093bdf4185d6984852c93ebbcab043884ff2
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.com>
2020-01-08 15:22:33 +0100

    mmpack-build - create ghost debian packages
    Ghost packages are virtual packages (that provide no file) that allow
    replacing a system package dependency with a mmpack package one, while
    wiring the redirections for the runtime execution.
    
    Changes to the mmpack specs:
     - new boolean "ghost" field
     - new dict "syspkg-srcnames" field
    
    general:
      ...
      ghost: True  # optional
      syspkg-srcnames: # optional
        debian: qtbase-opensource-src
      ...
    
    Signed-off-by: Nicolas Bourdaud <nicolas.bourdaud@mindmaze.com>
    Change-Id: I8c47642647cf247d52bfd3178f2d6e36659b723b

---
+++ tests/smoke-tests/test_mmpack-build_ghost.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -ex
+
+. $(dirname $0)/test-mmpack-build-common.sh
+
+build_sys=ghost
+
+env
+prepare_env $build_sys
+
+
+kill_http_server() {
+	test "$srvpid" && kill $srvpid
+	cleanup
+}
+trap kill_http_server EXIT
+
+
+# spawn HTTP server
+python3 -m http.server --bind 127.0.0.1 --directory $BUILDDIR/test-sysrepo 2>/dev/null &
+srvpid=$!
+
+
+#trap cleanup EXIT
+cleanup
+mmpack-build clean --wipe
+
+# generate specs for the current test
+gen-ghost-specs
+
+mkdir -p $TMP_BUILD
+pushd $TMP_BUILD
+export MMPACK_BUILD_DPKG_REPO="http://127.0.0.1:8000/debian testdata"
+export MMPACK_BUILD_MSYS2_REPO="http://127.0.0.1:8000"
+mmpack-build --debug
+popd
+
+
+# check that the packages created are correct
+ls $XDG_DATA_HOME/mmpack-packages/hello*.mmpack-manifest
+ls $XDG_DATA_HOME/mmpack-packages/hello*.mpk
+ls $XDG_DATA_HOME/mmpack-packages/libhello*.mpk
+ls $XDG_DATA_HOME/mmpack-packages/hello-devel*.mpk
+ls $XDG_DATA_HOME/mmpack-packages/hello*src.tar.*
