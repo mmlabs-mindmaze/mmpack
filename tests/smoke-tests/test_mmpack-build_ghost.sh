9479af27acfc561555d324181b7dccba76cb6c6a
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.com>
2020-01-08 15:22:33 +0100

    mmpack-build - create ghost debian packages
    Ghost packages are virtual packages (that provide no file) that allow
    replacing a system package dependency with a mmpack package one, while
    wiring the redirections for the runtime execution.
    
    Changes to the mmpack specs:
     - new boolean "ghost" field
     - new dict "syspkg-srcnames" field
    
    general:
      ...
      ghost: True  # optional
      syspkg-srcnames: # optional
        debian: qtbase-opensource-src
      ...
    
    Signed-off-by: Nicolas Bourdaud <nicolas.bourdaud@mindmaze.com>
    Change-Id: I8c47642647cf247d52bfd3178f2d6e36659b723b

---
+++ tests/smoke-tests/test_mmpack-build_ghost.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+set -ex
+
+. $(dirname $0)/test-mmpack-build-common.sh
+
+build_sys=ghost
+
+env
+prepare_env $build_sys
+
+
+kill_http_server() {
+	test "$srvpid" && kill $srvpid
+#	cleanup
+}
+trap kill_http_server EXIT
+
+
+get_unused_port() {
+	local port
+	port=8000
+	while netstat -atn | grep -q :$port
+	do
+		port=$(expr $port + 1)
+	done
+	echo $port
+}
+
+
+# spawn HTTP server
+http_port=$(get_unused_port)
+python3 -m http.server --bind 127.0.0.1 --directory $BUILDDIR/test-sysrepo $http_port 2>/dev/null &
+srvpid=$!
+
+
+#trap cleanup EXIT
+cleanup
+mmpack-build clean --wipe
+
+# generate specs for the current test
+gen-ghost-specs
+
+mkdir -p $TMP_BUILD
+pushd $TMP_BUILD
+export MMPACK_BUILD_DPKG_REPO="http://127.0.0.1:$http_port/debian testdata"
+export MMPACK_BUILD_MSYS2_REPO="http://127.0.0.1:$http_port"
+mmpack-build --debug
+popd
+
+
+pushd $XDG_DATA_HOME/mmpack-packages &> /dev/null
+
+# check that the expected files are created
+ls hello_1.0.0_*.mmpack-manifest
+ls hello_1.0.0_*.mpk
+ls libhello1_1.0.0_*.mpk
+ls hello-devel_1.0.0*.mpk
+ls hello_1.2.3_src.tar.*
+
+
+# Check that hello and hello-devel packages contains only package info and
+# sumsha file, ie, they are empty
+for pkgfile in hello_1.0.0_*.mpk hello-devel_1.0.0*.mpk
+do
+	pkgname=$(echo $pkgfile | sed s/_.*//)
+	cmp <(tar -tf $pkgfile) <<EOF
+./
+./MMPACK/
+./MMPACK/info
+./var/
+./var/lib/
+./var/lib/mmpack/
+./var/lib/mmpack/metadata/
+./var/lib/mmpack/metadata/$pkgname.sha256sums
+EOF
+
+done
+
+
+# Check that libhello1 contains only package info, sumsha and symbol files
+pkgfile=$(ls libhello1_1.0.0*.mpk)
+pkgname=$(echo $pkgfile | sed s/_.*//)
+cmp <(tar -tf $pkgfile) <<EOF
+./
+./MMPACK/
+./MMPACK/info
+./var/
+./var/lib/
+./var/lib/mmpack/
+./var/lib/mmpack/metadata/
+./var/lib/mmpack/metadata/$pkgname.sha256sums
+./var/lib/mmpack/metadata/$pkgname.symbols
+EOF
+
+
+popd &> /dev/null
