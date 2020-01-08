fdb3eb68512508e8f52e5b4a5bb12a5a00b559b5
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.com>
2020-01-08 15:22:33 +0100

    mmpack-build - create ghost debian packages
    Ghost packages are virtual packages (that provide no file) that allow
    replacing a system package dependency with a mmpack package one, while
    wiring the redirections for the runtime execution.
    
    Changes to the mmpack specs:
     - new boolean "ghost-syspkg" field
     - new dict "syspkg-srcnames" field
    
    general:
      ...
      ghost-syspkg: True  # optional
      syspkg-srcnames: # optional
        debian: qtbase-opensource-src
      ...
    
    Signed-off-by: Nicolas Bourdaud <nicolas.bourdaud@mindmaze.com>
    Change-Id: I8c47642647cf247d52bfd3178f2d6e36659b723b

--- src/mmpack-build/syspkg_manager_base.py
+++ src/mmpack-build/syspkg_manager_base.py
@@ -3,7 +3,38 @@
 Base class abstracting system package manager
 """
 
-from typing import List
+import os
+from typing import List, Dict
+
+from . workspace import cached_download
+from . mm_version import Version
+
+
+class SysPkg:
+    """
+    representation of a generic system package to download from repo
+    """
+    def __init__(self):
+        self.name = ''
+        self.version = ''
+        self.source = ''
+        self.filename = ''
+        self.url = ''
+        self.sha256 = ''
+
+    def get_sysdep(self) -> str:
+        """
+        get system dependency string corresponding to depends on the package
+        """
+        return self.name
+
+    def download(self, builddir) -> str:
+        """
+        get system dependency string corresponding to depends on the package
+        """
+        pkg_file = os.path.join(builddir, self.filename)
+        cached_download(self.url, pkg_file, self.sha256)
+        return pkg_file
 
 
 class SysPkgManager:
@@ -32,3 +63,65 @@ class SysPkgManager:
         Get sysdep providing the specified python package
         """
         raise NotImplementedError
+
+    def _parse_pkgindex(self, builddir: str, srcname: str) -> List[SysPkg]:
+        """
+        fetch and parse system repository index and generate the list of system
+        package that are created from the same source.
+
+        Args:
+            builddir: path where to download temporary files
+            srcname: name of source project
+
+        Return: List of system package description
+        """
+        raise NotImplementedError
+
+    def _get_mmpack_version(self, sys_version: str) -> Version:
+        """
+        Get a version usable for a mmpack package from system package version
+        """
+        # pylint: disable=no-self-use
+        return Version(sys_version)
+
+    def _extract_syspkg(self, pkgfile: str, unpackdir: str) -> List[str]:
+        """
+        Extract downloaded system package file
+
+        Args:
+            pkgfile: path to downloaded system package file
+            unpackdir: location where to unpack the downloaded system package
+
+        Return: list of files unpacked from the system package
+        """
+        raise NotImplementedError
+
+    def fetch_unpack(self, srcname: str, builddir: str,
+                     unpackdir: str) -> (str, Dict[str, str]):
+        """
+        Download and unpack system package matching the source name
+
+        Args:
+            srcname: name of the source used to list the binary packages
+            builddir: path where to download the temporary files (repo, pkgs)
+            unpackdir: path where to unpack the matching system packages
+
+        Return:
+            Couple of system package version as downloaded and the mapping of
+            file to package name that provides them
+        """
+        pkg_list = self._parse_pkgindex(builddir, srcname)
+        if not pkg_list:
+            raise FileNotFoundError('No system package matching {} found'
+                                    .format(srcname))
+
+        files_sysdep = dict()
+        for pkg in pkg_list:
+            # Download and extract the download system package file and update
+            # the mapping files -> syspkg
+            pkg_file = pkg.download(builddir)
+            files = self._extract_syspkg(pkg_file, unpackdir)
+            files_sysdep.update(dict.fromkeys(files, pkg.get_sysdep()))
+
+        version = self._get_mmpack_version(pkg_list[0].version)
+        return (version, files_sysdep)
