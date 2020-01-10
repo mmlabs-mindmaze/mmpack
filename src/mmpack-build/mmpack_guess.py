f64d0b86e7bc4d07b9ac755d3f0435a0c3a50268
Gabriel Ganne <gabriel.ganne@mindmaze.com>
2020-01-10 09:51:28 +0100

    mmpack-build - add mandatory license and copyright fields
    - licenses is a list for projects with multiple licenses.
      each license can be either a file in the source tree or
      the name of the license if known by mmpack (see common-licenses
      folder)
    - copyright can be either a string (to ease the simple cases) or a file.
    
    "licenses" is an optional key, but one that must be resolved for the
    package to be created. If not given, will fallback to either LICENSE or
    COPYING files, with a single license file.
    
    "copyright" is entirely optional.
    
    The current list of known "common" licenses are taken right out of
    Debian's base-files package. The license name simply is the name of the
    file.
    Licenses such as the MIT (which is fairly common) cannot be listed here
    because it requests the author name to be part of the license itself and
    therefore cannot be factorized this way.
    
    Change-Id: I20c3d16f797d8e0392da2c994fed7fc372acf4c7

--- src/mmpack-build/mmpack_guess.py
+++ src/mmpack-build/mmpack_guess.py
@@ -13,8 +13,11 @@ stdout.
 '''
 
 import glob
+import os
 import re
+from typing import List
 import yaml
+
 
 from . common import shell, ShellException
 
@@ -113,6 +116,47 @@ def guess_description() -> str:
         return UNKNOWN
 
 
+def guess_copyright() -> str:
+    """
+    guess project copyright
+
+    Assume a "src" folder, take the first file it can find inside
+    and return the first line containing the term "copyright" from its
+    beginning
+    """
+    is_copyright = re.compile(r'copyright', re.IGNORECASE)
+    ntries = 10
+    try:
+        for src in glob.glob('src/**', recursive=True):
+            if os.path.isfile(src):
+                header = open(src).readlines()[0:10]
+                for line in header:
+                    if is_copyright.search(line):
+                        return line
+                if ntries > 0:
+                    ntries -= 1
+                    continue
+                break
+        return UNKNOWN
+    except Exception:
+        return UNKNOWN
+
+
+def guess_licenses() -> List[str]:
+    """
+    guess project license from a license file (case insensitive)
+    Assuming a single license file at the top of the tree
+    """
+    is_license = re.compile(r'(LICENSE|COPYING)', re.IGNORECASE)
+    try:
+        for entry in os.listdir():
+            if os.path.isfile(entry) and is_license.match(entry):
+                return entry
+        return UNKNOWN
+    except Exception:
+        return list(UNKNOWN)
+
+
 def main(argv):  # pylint: disable=unused-argument
     """
     guess mmpack specs and print to stdout
@@ -122,7 +166,10 @@ def main(argv):  # pylint: disable=unused-argument
         'version': guess_version(),
         'maintainer': guess_maintainer(),
         'url': guess_url(),
-        'description': guess_description()}}
+        'description': guess_description(),
+        'copyright': guess_copyright(),
+        'licenses': guess_licenses(),
+    }}
     specs_str = yaml.dump(specs, default_flow_style=False,
                           allow_unicode=True, indent=4)
     for line in specs_str.split('\n'):
