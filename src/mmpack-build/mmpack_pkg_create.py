19a14612a34fbdf7cb4383186c356814d079fdd1
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.com>
2020-01-14 10:28:27 +0100

    mmpack-build: rework pkg-create interface
    Now pkg-create accept an optional positional argument. This specifies
    the path to source package to build (either git url, path, tarball or
    mmpack source tarball). If not specified, the current folder and its
    parent will be searched recursively for a mmpack folder containing
    specs file.
    
    The type of argument is guessed. The guess can be overriden by the
    '--git', '--path', '--src' or '--mmpack-src' flag.
    
    Change-Id: Ia2d06a5090cde6536b131ed241b7a84e41ff3ac5

--- src/mmpack-build/mmpack_pkg_create.py
+++ src/mmpack-build/mmpack_pkg_create.py
@@ -4,19 +4,24 @@ Create a mmpack package
 
 Usage:
 
-mmpack pkg-create [--git-url <path or url of a git repo> | --src <tarball> |
-                   --mmpack-src <mmpack_source_tarball>]
+mmpack pkg-create [--git|--src|--mmpack-src|--path]
                   [--tag <tag>] [--prefix <prefix>] [--skip-build-tests]
+                  [path_or_url]
 
-If neither git url or source tarball was given, look through the tree for a
-mmpack folder, and use the containing folder as root directory.
+Create mmpack package specified in argument. If no argument is provided, look
+through the tree for a mmpack folder, and use the containing folder as root
+directory.
 
-Otherwise, if git repository url is provided, clone the project from given
-git url. If a tag was specified, use it (checkout on the given tag).
+The type of argument is guessed and the appropriate create method will be used.
+However the method used can be forced using the flags:
+ '--git': assumes a git url or path to a local git repo
+ '--src': assumes a source tarball
+ '--path': assumes path to a mmpack packaging
+ '--mmpack-src': assumes argument is a mmpack source tarball
 
-If source tarball is specified, build packages from the unpacked archive.
-If tag is specified, it will indicated the name of sub folder in the cache
-to use to build the packages.
+If tag is specified, it will be used to use the specified branch/tag in case of
+git method. Additionally, for all method, it will indicate the name of sub
+folder in the cache to use to build the packages.
 
 If a prefix is given, work within it instead.
 
@@ -25,12 +30,14 @@ Examples:
 $ mmpack pkg-create
 
 # From anywhere
-$ mmpack pkg-create --url ssh://git@intranet.mindmaze.ch:7999/~user/XXX.git \
-                    --tag v1.0.0-custom-tag-target
+$ mmpack pkg-create --tag v1.0.0-custom-tag-target \
+ssh://git@intranet.mindmaze.ch:7999/~user/XXX.git
 """
 
 import os
+import tarfile
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from subprocess import call
 
 from . common import set_log_file
 from . src_package import SrcPackage
@@ -48,18 +55,18 @@ def parse_options(argv):
     parser = ArgumentParser(description=__doc__,
                             formatter_class=RawDescriptionHelpFormatter)
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('--git-url',
-                       action='store', dest='url', type=str,
-                       help='project git url/path')
-    group.add_argument('--path',
-                       action='store', dest='path', type=str,
-                       help='path to folder having a mmpack packaging')
-    group.add_argument('--src',
-                       action='store', dest='srctar', type=str,
-                       help='source package tarball')
-    group.add_argument('--mmpack-src',
-                       action='store', dest='mmpack_srctar', type=str,
-                       help='mmpack source package tarball')
+    group.add_argument('--git', dest='method',
+                       action='store_const', const='git',
+                       help='interpret argument as url/path to git repository')
+    group.add_argument('--path', dest='method',
+                       action='store_const', const='path',
+                       help='interpret argument as path to mmpack packaging')
+    group.add_argument('--src', dest='method',
+                       action='store_const', const='tar',
+                       help='interpret argument as source tarball')
+    group.add_argument('--mmpack-src', dest='method',
+                       action='store_const', const='srcpkg',
+                       help='interpret argument as mmpack source tarball')
     parser.add_argument('-t', '--tag',
                         action='store', dest='tag', type=str,
                         help='project tag')
@@ -75,12 +82,13 @@ def parse_options(argv):
     parser.add_argument('-y', '--yes',
                         action='store_true', dest='assumeyes',
                         help='always assume yes to any prompted question')
+    parser.add_argument('path_or_url', nargs='?',
+                        help='path or url to project providing mmpack package')
     args = parser.parse_args(argv)
 
-    if not args.url and not args.srctar and not args.mmpack_srctar \
-       and not args.path:
-        args.url = find_project_root_folder()
-        if not args.url:
+    if not args.path_or_url:
+        args.path_or_url = find_project_root_folder()
+        if not args.path_or_url:
             raise ValueError('did not find project to package')
 
     # set workspace prefix
@@ -95,26 +103,46 @@ def parse_options(argv):
     return args
 
 
+def _is_git_dir(path: str) -> bool:
+    if os.path.isdir(path + '/.git'):
+        return True
+
+    return call(['git', '--git-dir='+path, 'rev-parse', '--git-dir'])
+
+
+def _is_tarball_file(path: str) -> bool:
+    try:
+        with tarfile.open(path, 'r') as tar:
+            return 'mmpack/specs' in tar.getnames()
+    #pylint: disable=broad-except
+    except Exception:
+        return False
+
+
+def _guess_method(path_or_url: str) -> str:
+    if _is_git_dir(path_or_url):
+        return 'git'
+    elif os.path.isdir(path_or_url):
+        return 'path'
+    elif _is_tarball_file(path_or_url):
+        if path_or_url.endswith('_src.tar.xz'):
+            return 'srcpkg'
+        else:
+            return 'tar'
+
+    # If nothing has worked before, lets assume this is a git remote url
+    return 'git'
+
+
 def main(argv):
     """
     entry point to create a mmpack package
     """
     args = parse_options(argv[1:])
+    if not args.method:
+        args.method = _guess_method(args.path_or_url)
 
-    if args.url:
-        method = 'git'
-        path_url = args.url
-    elif args.srctar:
-        method = 'tar'
-        path_url = args.srctar
-    elif args.mmpack_srctar:
-        method = 'srcpkg'
-        path_url = args.mmpack_srctar
-    elif args.path:
-        method = 'path'
-        path_url = args.path
-
-    srctarball = SourceTarball(method, path_url, args.tag)
+    srctarball = SourceTarball(args.method, args.path_or_url, args.tag)
     srctarball.prepare_binpkg_build()
 
     specfile = os.path.join(srctarball.detach_srcdir(), 'mmpack/specs')
