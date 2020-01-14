4aab487716e30ff713e9712bf265a5f1e5d60462
Marjorie Bournat <marjorie.bournat@mindmaze.com>
2020-01-14 14:12:01 +0100

    mmpack: mmpack rdepends uses of the constraints
    Before the command rdepends needed to define some options, now it uses
    the constraints structure to perform the filtering on the packages.
    
    Change-Id: I50098e4f3b0fcb538cceb8df122bec1e17a74b51

--- src/mmpack/mmpack-rdepends.c
+++ src/mmpack/mmpack-rdepends.c
@@ -24,17 +24,10 @@
 
 
 static int recursive = 0;
-static int sumsha = 0;
-static const char* repo_name = NULL;
 
 static const struct mmarg_opt cmdline_optv[] = {
-	{"repo", MMOPT_NEEDSTR, NULL, {.sptr = &repo_name},
-	 "Specify @REPO_NAME as the address of package repository"},
 	{"r|recursive", MMOPT_NOVAL|MMOPT_INT, "1", {.iptr = &recursive},
 	 "Print recursively the reverse dependencies"},
-	{"sumsha", MMOPT_NOVAL|MMOPT_INT, "1", {.iptr = &sumsha},
-	 "Search the reverse dependencies of the package referenced thanks to "
-	 "its sumsha"},
 };
 
 
@@ -142,6 +135,7 @@ LOCAL_SYMBOL
 int mmpack_rdepends(struct mmpack_ctx * ctx, int argc, const char* argv[])
 {
 	struct mmpkg const* pkg;
+	struct repolist_elt * repo;
 	int arg_index, rv = -1;
 	struct list_pkgs * rdep_list = NULL;
 
@@ -171,16 +165,11 @@ int mmpack_rdepends(struct mmpack_ctx * ctx, int argc, const char* argv[])
 	if (mmpack_ctx_use_prefix(ctx, 0))
 		return -1;
 
-	if (!sumsha) {
-		if ((pkg = parse_pkg(ctx, argv[arg_index])) == NULL)
-			return -1;
-	} else {
-		if (!(pkg = find_package_by_sumsha(ctx, argv[arg_index])))
-			return -1;
-	}
+	if ((pkg = parse_pkg(ctx, argv[arg_index])) == NULL)
+		return -1;
 
-
-	if (find_reverse_dependencies(ctx->binindex, pkg, repo_name,
+	repo = pkg->from_repo->repo;
+	if (find_reverse_dependencies(ctx->binindex, pkg, repo->name,
 	                              &rdep_list)) {
 		printf("No package found\n");
 		goto exit;
