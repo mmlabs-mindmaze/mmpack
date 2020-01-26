78bddfd8dbbfbea197b8d98b2d67c498a2541352
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2020-01-26 16:23:44 +0100

    mmpack-build: add test for parse_soname
    Change-Id: I44b6e263f54d31072dff466275f34362afdec524

--- tests/test_common.py
+++ tests/test_common.py
@@ -5,7 +5,7 @@ from os import makedirs, getcwd, chdir
 from os.path import dirname, abspath
 from shutil import rmtree
 
-from mmpack_build.common import list_files
+from mmpack_build.common import list_files, parse_soname, shlib_keyname
 
 
 REF_FILELIST = [
@@ -35,6 +35,16 @@ REF_FILELIST_WITHDIRS = [
 ]
 
 TEST_TREE_ROOT = 'test_tree/root'
+
+
+REF_SONAME_DATA = [
+    ('libfoo.so.1', 'libfoo', '1', 'libfoo1'),
+    ('libfoo-1.dll', 'libfoo', '1', 'libfoo1'),
+    ('libfoo.so.1.2', 'libfoo', '1.2', 'libfoo1.2'),
+    ('libfoo-1.2.dll', 'libfoo', '1.2', 'libfoo1.2'),
+    ('libfoo1.2.so.3', 'libfoo1.2', '3', 'libfoo1.2-3'),
+    ('libfoo1.2-3.dll', 'libfoo1.2', '3', 'libfoo1.2-3'),
+]
 
 
 class TestFileList(unittest.TestCase):
@@ -99,3 +109,19 @@ class TestFileList(unittest.TestCase):
         filelist = list_files(abspath(TEST_TREE_ROOT), exclude_dirs=True)
         self.assertEqual(filelist, REF_FILELIST)
 
+    def test_soname_parsing(self):
+        """
+        test parse_soname()
+        """
+        for ref_soname, ref_name, ref_version, _ in REF_SONAME_DATA:
+            name, version = parse_soname(ref_soname)
+            self.assertEqual(name, ref_name)
+            self.assertEqual(version, ref_version)
+
+    def test_shlib_name(self):
+        """
+        test shlib_keyname()
+        """
+        for ref_soname, _, _, ref_pkgname in REF_SONAME_DATA:
+            pkgname = shlib_keyname(ref_soname)
+            self.assertEqual(pkgname, ref_pkgname)
