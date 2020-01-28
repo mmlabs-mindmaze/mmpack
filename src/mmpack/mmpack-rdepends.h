d073aa5d261b1724f72ceeba768cae02d01bf2c3
Marjorie Bournat <marjorie.bournat@mindmaze.com>
2020-01-28 16:07:32 +0100

    mmpack: add autoremove command
    This commands remove installed packages that are not necessary anymore.
    More precisely it removes packages that were automatically installed
    such that no manually installed packages depend on them.
    
    Change-Id: Icb2c78870d3bf4150bd65d29f677f3dd53afec98

--- src/mmpack/mmpack-rdepends.h
+++ src/mmpack/mmpack-rdepends.h
@@ -10,6 +10,21 @@
 #define RDEPENDS_SYNOPSIS \
 	"rdepends [-r|--recursive] [--repo<=repo_name>] <package>[=[key:]<value>]"
 
+
+struct list_pkgs {
+	struct mmpkg const * pkg;
+	struct list_pkgs * next;
+};
+
+void list_pkgs_add_elt(struct list_pkgs ** list, struct mmpkg const * pkg);
+int list_pkgs_search_elt(struct list_pkgs * list, struct mmpkg const * pkg);
+void list_pkgs_destroy_all_elt(struct list_pkgs ** list);
+int find_reverse_dependencies(struct binindex binindex,
+                              struct mmpkg const* pkg,
+                              const struct repolist_elt * repo,
+                              struct list_pkgs** rdep_list,
+                              int rec);
+
 int mmpack_rdepends(struct mmpack_ctx * ctx, int argc, char const ** argv);
 
 #endif /* MMPACK_RDEPENDS_H */
