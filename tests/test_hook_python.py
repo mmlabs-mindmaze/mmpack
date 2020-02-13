42f3a0ccd36280950f76c798484724b6578add0b
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.com>
2020-02-13 11:58:44 +0100

    mmpack-build: setuptool script generates dependency
    When building python package with setup.py, setuptools generates
    launcher script using pkg_resources.load_entry_point function. Normally
    mmpack package containing launcher script must depend on the mmpack
    package containing the python __main__.py module. This was not the case
    because the inspection of the launcher did not detect its use of public
    python package.
    
    python_provides.py has been modified to declare the __main__ if the
    module __main__.py is present. On the other hand, python_depends now
    infer the module loaded by a call to pkg_resources.load_entry_point.
    
    Change-Id: Icc6c34a20974aac723705ea42ef2768acbfdc8f5

--- tests/test_hook_python.py
+++ tests/test_hook_python.py
@@ -68,6 +68,7 @@ class TestPythonHook(unittest.TestCase):
     def test_provides_multi(self):
         """test provides pkg with multiple modules"""
         pkgfiles = [
+            'multi/__main__.py',
             'multi/__init__.py',
             'multi/foo.py',
             'multi/bar.py',
@@ -94,6 +95,7 @@ class TestPythonHook(unittest.TestCase):
             'multi.bar.Bar.__init__': 'multi..bar.Bar.__init__',
             'multi.bar.Bar.drink': 'multi..bar.Bar.drink',
             'multi.bar.A_BAR': '',
+            'multi.__main__': '',
         }
         syms = _load_py_symbols('multi', pkgfiles)
         self.assertEqual(syms, refsymbols)
@@ -122,6 +124,7 @@ class TestPythonHook(unittest.TestCase):
     def test_depends_import_multi(self):
         """test dependent import with multiple modules"""
         pkgfiles = [
+            'multi/__main__.py',
             'multi/__init__.py',
             'multi/foo.py',
             'multi/bar.py',
@@ -137,6 +140,16 @@ class TestPythonHook(unittest.TestCase):
             'simple.main_dummy_fn',
             'simple.MainData.__init__',
             'simple.MainData.disclose_private',
+        }
+        imports = _get_py_depends(pkgfiles)
+        self.assertEqual(imports, refimports)
+
+    def test_depends_launcher(self):
+        """test dependent imports with simple package with no import"""
+        pkgfiles = ['launcher']
+        refimports = {
+            'multi.__main__',
+            'pkg_resources.load_entry_point',
         }
         imports = _get_py_depends(pkgfiles)
         self.assertEqual(imports, refimports)
