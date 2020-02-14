c4aacaaf64b7dc5f6e2c7d62b38a3fa467bd5605
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.com>
2020-02-14 16:46:48 +0100

    mmpack-build: fix python_depends method call inspection
    With recent version of astroid (the inference lib for python code),
    inferring the called function definition from a class method call can
    lead to some inference error. That would lead to some python used symbol
    being unnoticed.
    
    This is fixed by inferring first the instance of the class and then
    inferring the used method.
    
    Change-Id: Iae6ca04af0bb064a09289e4b6639ff81345cf59b

--- src/mmpack-build/python_depends.py
+++ src/mmpack-build/python_depends.py
@@ -18,13 +18,14 @@ not the name as declared for example in the __init__.py.
 import sys
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from os.path import abspath
-from typing import Set
+from typing import Set, Iterator
 
 from astroid import MANAGER as astroid_manager
-from astroid import Uninferable
-from astroid.exceptions import InferenceError, NameInferenceError
+from astroid import Uninferable, FunctionDef
+from astroid.exceptions import InferenceError, NameInferenceError, \
+    AttributeInferenceError
 from astroid.modutils import is_standard_module
-from astroid.node_classes import Call
+from astroid.node_classes import Call, Attribute
 from mmpack_build.file_utils import is_python_script
 
 
@@ -36,9 +37,23 @@ def _is_module_packaged(mod, pkgfiles: Set[str]) -> bool:
     return mod.path and mod.path[0] in pkgfiles
 
 
+def _call_func_iter(call: Call) -> Iterator[FunctionDef]:
+    """
+    equivalent to call.func.infer() supporting recent version of astroid
+    """
+    if isinstance(call.func, Attribute):
+        attr = call.func
+        for base in attr.expr.infer():
+            for funcdef in base.igetattr(attr.attrname):
+                yield funcdef
+    else:
+        for funcdef in call.func.infer():
+            yield funcdef
+
+
 def _inspect_call(call: Call, used_symbols: Set[str], pkgfiles: Set[str]):
     try:
-        for funcdef in call.func.infer():
+        for funcdef in _call_func_iter(call):
             orig_mod = funcdef.root()
 
             # ignore inferred call that does not generate dependencies
@@ -53,7 +68,7 @@ def _inspect_call(call: Call, used_symbols: Set[str], pkgfiles: Set[str]):
     # As python is a dynamic language, uninferable name lookup or uninferable
     # object can be common (when it highly depends on the context that we
     # cannot have without executing the code) Hence, it is safer to ignore.
-    except (NameInferenceError, InferenceError):
+    except (NameInferenceError, InferenceError, AttributeInferenceError):
         pass
 
 
