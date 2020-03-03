6f19ca9f48b5f89e059fb7d962f4c0dd52257b38
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2020-03-03 08:29:41 +0100

    mmpack-build: move PackageInfo in dedicated module
    Change-Id: Ifb61e461dd2831c931aad9cb9d840e0f9b03c066

--- src/mmpack-build/base_hook.py
+++ src/mmpack-build/base_hook.py
@@ -5,37 +5,7 @@ template to create mmpack-build hook
 
 from typing import Set, Dict, List
 
-from . mm_version import Version
-
-
-class PackageInfo:
-    """
-    container to share package data with hooks
-    """
-
-    def __init__(self, name: str):
-        self.name = name
-        self.files = set()
-        self.provides = dict()
-        self.deplist = []  # List of triplet of (dep_name, min_ver, max_ver)
-        self.version = Version('any')
-        self.sysdeps = set()
-        self.ghost = False
-
-    def add_to_deplist(self, pkgname: str,
-                       minver: Version = Version('any'),
-                       maxver: Version = Version('any')):
-        """
-        add a mmpack dependency optionally with a minimal and maximal
-        version
-        """
-        self.deplist.append((pkgname, minver, maxver))
-
-    def add_sysdep(self, sysdep: str):
-        """
-        add a system dependency
-        """
-        self.sysdeps.add(sysdep)
+from . package_info import PackageInfo
 
 
 class BaseHook:
