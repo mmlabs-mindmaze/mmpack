196c3c87c34037b865c7ea1bc3d8ba3acef3923c
Marjorie Bournat <marjorie.bournat@mindmaze.com>
2020-03-10 10:32:25 +0100

    mmpack: add srcindex
    This commit adds a structure source index. This structure is filled
    thanks to the fields contained in the source-index file of the
    repository.
    
    Change-Id: I64d3ce70bfbcd5c040c43ece0c7981e213817495

---
+++ src/mmpack/srcindex.h
@@ -0,0 +1,30 @@
+/*
+ * @mindmaze_header@
+ */
+#ifndef SRCINDEX_H
+#define SRCINDEX_H
+
+#include "indextable.h"
+#include "settings.h"
+
+struct srcpkg {
+	mmstr * name;
+	mmstr * filename;
+	mmstr * sha256;
+	mmstr * version;
+	size_t size;
+	int name_id;
+	struct repolist_elt * repo;
+};
+
+struct srcindex {
+	struct indextable pkgname_idx;
+	int num_pkgname;
+};
+
+void srcindex_init(struct srcindex* srcindex);
+void srcindex_deinit(struct srcindex* srcindex);
+int srcindex_populate(struct srcindex * srcindex, char const * index_filename,
+                      struct repolist_elt * repo);
+
+#endif /* SRCINDEX_H */
