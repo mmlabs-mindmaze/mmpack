07952a5e9eac9b8f8bc0412a2d5d3d5efef70e40
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.com>
2020-08-03 16:12:55 +0200

    add srcindex unit tests
    Change-Id: I66d2e2312954fa10e0f0cb2d3b9660d6334d77e9

---
+++ tests/srcindex_test.c
@@ -0,0 +1,59 @@
+/*
+ * @mindmaze_header@
+ */
+
+#if defined (HAVE_CONFIG_H)
+# include <config.h>
+#endif
+
+#include <check.h>
+
+#include "srcindex.h"
+#include "testcases.h"
+
+#define TEST_SRCINDEX_DIR SRCDIR"/tests/source-indexes"
+
+static struct srcindex source_index;
+
+static const char * srcindexes[] = {
+	TEST_SRCINDEX_DIR"/simple",
+};
+#define NUM_SRCINDEXES MM_NELEM(srcindexes)
+
+static
+void ctx_setup(void)
+{
+	srcindex_init(&source_index);
+}
+
+static
+void ctx_teardown(void)
+{
+	srcindex_deinit(&source_index);
+}
+
+START_TEST(test_srcindex_parsing)
+{
+	int rv;
+	struct repo repo = {
+		.url = mmstr_alloca_from_cstr("fake://url"),
+		.name = mmstr_alloca_from_cstr("repo"),
+	};
+
+	rv = srcindex_populate(&source_index, srcindexes[_i], &repo);
+	ck_assert(rv == 0);
+}
+END_TEST
+
+
+TCase* create_srcindex_tcase(void)
+{
+	TCase * tc;
+
+	tc = tcase_create("srcindex");
+	tcase_add_checked_fixture(tc, ctx_setup, ctx_teardown);
+
+	tcase_add_loop_test(tc, test_srcindex_parsing, 0, NUM_SRCINDEXES);
+
+	return tc;
+}
