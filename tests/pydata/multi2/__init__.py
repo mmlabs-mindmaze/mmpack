5e2498f82925a03b2f9012e9094079a350fe3827
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2020-12-27 20:53:19 +0100

    mmpack-build: python provides module beside __init__
    When detecting the symbols provided by a python package, all module
    located beside __init__.py in the package file hierachy are now
    provided. Previously only the module referenced directly or indirectly
    by __init__ were added to the provided symbols.
    
    Change-Id: I844adc5d9cbc9b5ddcf651d3180adb4bb4383d36

---
+++ tests/pydata/multi2/__init__.py
@@ -0,0 +1,22 @@
+# @mindmaze_header@
+"""
+python package that is made of several modules with __init__.py
+"""
+
+from . foo import somefunc, MainData, THE_ANSWER as _THE_ANSWER
+
+
+def _print_answer_multi():
+    print(_THE_ANSWER)
+
+
+
+class FooBar(MainData):
+    def __init__(self):
+        super().__init__('somedata')
+        self.new_data = somefunc(42)
+        self.new_data.v1 = 66
+        self.fullname = 'John Doe'
+
+    def hello(self):
+        print('hello')
