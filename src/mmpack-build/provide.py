99078c2a6f67d25c76e7fc7397cfa505de5fa6e0
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2021-03-18 23:06:14 +0100

    mmpack-build: support for compressed symbols files
    Change-Id: Ie7d8e9c1d03799d935fa30c90053b562897baa71

--- src/mmpack-build/provide.py
+++ src/mmpack-build/provide.py
@@ -3,7 +3,8 @@
 common classes to specify provided symbols to other packages
 """
 
-from glob import glob
+import re
+from os import listdir
 from typing import Set, Dict, Tuple, List
 
 from . common import wprint, yaml_serialize, yaml_load
@@ -277,11 +278,12 @@ def load_mmpack_provides(extension: str, symtype) -> ProvideList:
         installed mmpack packages matching symtype
     """
     wrk = Workspace()
-    symfiles = glob('{}/var/lib/mmpack/metadata/**.{}'
-                    .format(wrk.prefix, extension))
+    metadatadir = wrk.prefix + '/var/lib/mmpack/metadata/'
+    pattern = re.compile(r'.*.{}(?:.gz)?'.format(extension))
 
     provides = ProvideList(symtype)
-    for symfile in symfiles:
-        provides.add_from_file(symfile)
+    for symfile in listdir(metadatadir):
+        if pattern.fullmatch(symfile):
+            provides.add_from_file(metadatadir + symfile)
 
     return provides
