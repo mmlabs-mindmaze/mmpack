e67f33f621c02bb60708733e0994e98903f04b8d
Nicolas Bourdaud <nicolas.bourdaud@alice-bob.com>
2021-04-26 12:59:33 +0200

    mmpack: binpkg in its own compilation unit
    Move binpkg related implementation in binpkg.h and binpkg.c
    
    Change-Id: I835884c1a8a4411842105d150d6ec68fc00c2002

---
+++ src/mmpack/binpkg.h
@@ -0,0 +1,83 @@
+/*
+ * @mindmaze_header@
+ */
+#ifndef BINPKG_H
+#define BINPKG_H
+
+#include "mmstring.h"
+#include "repo.h"
+#include "strlist.h"
+
+
+#define MMPKG_FLAGS_GHOST       (1 << 0)
+
+
+struct pkgdep {
+	mmstr const * name;
+	mmstr const * min_version; /* inclusive */
+	mmstr const * max_version; /* exclusive */
+
+	struct pkgdep * next;
+};
+
+struct pkgdep* pkgdep_create(char const * name);
+void pkgdep_destroy(struct pkgdep * dep);
+
+
+struct binpkg {
+	int name_id;
+	mmstr const * name;
+	mmstr const * version;
+	mmstr const * source;
+	mmstr const * desc;
+	mmstr const * sumsha;
+	mmstr const * srcsha;
+
+	struct remote_resource* remote_res;
+
+	int flags;
+
+	struct pkgdep* mpkdeps;
+	struct strlist sysdeps;
+	struct compiled_dep* compdep;
+};
+
+
+
+void binpkg_init(struct binpkg* pkg, const mmstr* name);
+void binpkg_deinit(struct binpkg * pkg);
+void binpkg_save_to_index(struct binpkg const * pkg, FILE* fp);
+int binpkg_check_valid(struct binpkg const * pkg, int in_repo_cache);
+int binpkg_is_provided_by_repo(struct binpkg const * pkg,
+                               const struct repo* repo);
+struct remote_resource* binpkg_get_or_create_remote_res(struct binpkg* pkg,
+                                                        const struct repo* repo);
+void binpkg_add_remote_resource(struct binpkg* pkg,
+                                struct remote_resource* res_added);
+
+
+static inline
+void binpkg_update_flags(struct binpkg* pkg, int mask, int set)
+{
+	if (set)
+		pkg->flags |= mask;
+	else
+		pkg->flags &= ~mask;
+}
+
+
+static inline
+int binpkg_is_ghost(struct binpkg const * pkg)
+{
+	return pkg->flags & MMPKG_FLAGS_GHOST;
+}
+
+
+static inline
+int binpkg_is_available(struct binpkg const * pkg)
+{
+	return pkg->remote_res != NULL;
+}
+
+
+#endif /* BINPKG_H */
