353ba4d1612c6e211c07ff2a24b26d8d6ff57efe
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2021-04-29 14:51:10 +0200

    mmpack: add strchunk_extent()
    Change-Id: Ib9283d00e48fcdac2d95382a09a479d345795b0a

--- src/mmpack/strchunk.h
+++ src/mmpack/strchunk.h
@@ -190,6 +190,35 @@ bool strchunk_is_whitespace(struct strchunk sc)
 }
 
 
+/**
+ * strchunk_extent() - get the minimal chunk that contains 2 string chunks
+ * @sc1:        first chunk to merge
+ * @sc2:        second chunk to merge
+ *
+ * Find the smallest region that will contain both @sc1 and @sc2. If the two
+ * are disjoint, the resulting region may contains characters neither
+ * referenced by @sc1 or @sc2.
+ *
+ * Returns: the smallest string chunk that overlaps both @sc1 and @sc2
+ */
+static inline
+struct strchunk strchunk_extent(struct strchunk sc1, struct strchunk sc2)
+{
+	const char *start, *end;
+
+	if (sc2.len == 0)
+		return sc1;
+
+	if (sc1.len == 0)
+		return sc2;
+
+	start = MIN(sc1.buf, sc2.buf);
+	end = MAX(sc1.buf + sc1.len, sc2.buf + sc2.len);
+
+	return (struct strchunk) {.buf = start, .len = end - start};
+}
+
+
 static inline
 bool strchunk_equal(struct strchunk sc, const char* str)
 {
