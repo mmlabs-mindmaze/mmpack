40f1584fa479d75e828c2cbaca5280cc0bde044c
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2021-07-06 16:17:13 +0200

    Add site.py WIP
    Change-Id: I0a0669f5e3f58685f831a617207fd324ebe9ac69

---
+++ src/mmpack/site.py
@@ -0,0 +1,7 @@
+try:
+    from os import add_dll_directory
+    from os.path import abspath, join
+
+    _MMPACK_DLL_DIR = add_dll_directory(abspath(join(__file__, '../../../../bin')))
+except ImportError:
+    pass
