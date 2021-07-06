02b16a02d4be0cb414e1e50f2349c270a50c90c9
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2021-07-06 16:17:13 +0200

    mmpack: prefix site.py allows to use prefix libs
    Add startup code in prefix site.py to ensure that libraries installed in
    prefix are searched when looking for runtime dependencies. This is
    achieved by monkey patch cdll.LoadLibrary when on Linux or adding
    <prefix>/lib to dll search on windows (needed since python 3.8 on
    windows).
    
    Change-Id: I4e2b728de8876a45abfdc51c2c5ce79aaa236a62

--- src/mmpack/site.py
+++ src/mmpack/site.py
@@ -1,8 +1,34 @@
 # @mindmaze_header@
 
 from importlib.util import spec_from_file_location, module_from_spec
-from os.path import join
+from os.path import abspath, join, exists
+from sys import platform
 from sysconfig import get_paths
+
+
+# Make mmpack prefix folders usable searchable for libraries
+if platform == 'linux':
+    # Monkey ctypes in order to cdll.LoadLibrary search in prefix libdir
+    from ctypes import _dlopen
+
+    _libdir = abspath(join(__file__, '../../..'))
+
+    def _replace_dlopen(name, mode):
+        path = join(_libdir, name)
+        if '/' not in name and exists(path):
+            return _dlopen(path, mode)
+        return _dlopen(name, mode)
+
+    import ctypes
+    ctypes._dlopen = _replace_dlopen
+
+elif platform == 'win32':
+    try:
+        from os import add_dll_directory
+        _prefix_bindir = abspath(join(__file__, '../../../../bin'))
+        _MMPACK_DLL_DIR = add_dll_directory(_prefix_bindir)
+    except (ImportError, FileNotFoundError):
+        pass
 
 
 # Make this site module behave as a proxy to the standard site mode
