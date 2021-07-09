23bd3dc7bb917c5f04c0dfc58c7d9c518ed35f16
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2021-07-10 00:26:08 +0200

    FIx site WOP
    Change-Id: I64bdeae1b2d732b0a240876ea55adc57c8b112c3

--- src/mmpack/site.py
+++ src/mmpack/site.py
@@ -1,7 +1,15 @@
-try:
-    from os import add_dll_directory
-    from os.path import abspath, join
+from os.path import abspath, join
+from sys import platform
 
-    _MMPACK_DLL_DIR = add_dll_directory(abspath(join(__file__, '../../../../bin')))
-except ImportError:
-    pass
+_prefix_bindir = abspath(join(__file__, '../../../../bin')
+
+if platform == 'linux':
+    from os import environ
+    environ['LD_LIBRARY_PATH'] = ':'.join(environ.get('LD_LIBRARY_PATH', [])
+                                          + [_prefix_bindir])
+elif platform == 'win32':
+    try:
+        from os import add_dll_directory
+        _MMPACK_DLL_DIR = add_dll_directory())
+    except ImportError:
+        pass
