755e3843f9b29fac061ef40be2c5421223f14df7
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-02-08 00:09:37 +0100

    mmpack: source subcmd unpack in src subdir
    When calling mmapck source subcmd, the associated mmpack source is
    download and installed in a subfolder of <prefix>/src named after the
    name of source, its version and the srcsh256.
    
    Change-Id: I518652d0d50333f1978ad39495ea873c407d9696

--- src/mmpack/mmpack-source.c
+++ src/mmpack/mmpack-source.c
@@ -21,27 +21,44 @@
 #include "package-utils.h"
 #include "repo.h"
 #include "srcindex.h"
+#include "tar.h"
 #include "utils.h"
 
 
 static
-int download_pkg_sources(struct mmpack_ctx * ctx, struct binpkg const * pkg)
+int install_pkg_sources(struct mmpack_ctx * ctx, struct binpkg const * pkg)
 {
 	const struct srcpkg* srcpkg;
-	mmstr* dst;
-	int rv;
+	const mmstr* cachedir;
+	mmstr *basepath, *srctar, *srcdir;
+	int rv = 0;
 
 	srcpkg = srcindex_lookup(&ctx->srcindex,
 	                         pkg->source, pkg->version, pkg->srcsha);
 	if (!srcpkg) {
-		printf("Cannot source of package %s %s (%s)\n",
+		printf("Cannot find source of package %s %s (%s)\n",
 		       pkg->source, pkg->version, pkg->srcsha);
 		return -1;
 	}
 
-	dst = mmstr_basename(NULL, srcpkg->remote_res->filename);
-	rv = download_remote_resource(ctx, srcpkg->remote_res, dst);
-	mmstr_free(dst);
+	cachedir = mmpack_ctx_get_pkgcachedir(ctx);
+	basepath = mmstr_basename(NULL, srcpkg->remote_res->filename);
+	srctar = mmstr_join_path_realloc(NULL, cachedir, basepath);
+	srcdir = mmstr_asprintf(NULL, "%s/src/%s-%s-%.4s", ctx->prefix,
+	                        srcpkg->name, srcpkg->version, srcpkg->sha256);
+
+	if (download_remote_resource(ctx, srcpkg->remote_res, srctar)
+	    || mm_mkdir(srcdir, 0777, MM_RECURSIVE)
+	    || tar_extract_all(srctar, srcdir)) {
+		printf("Failed to install sources: %s\n", mm_get_lasterror_desc());
+		rv = -1;
+	} else {
+		printf("Extracted the source to %s\n", srcdir);
+	}
+
+	mmstr_free(srctar);
+	mmstr_free(basepath);
+	mmstr_free(srcdir);
 
 	return rv;
 }
@@ -84,5 +101,5 @@ int mmpack_source(struct mmpack_ctx * ctx, int argc, const char* argv[])
 	if ((pkg = parse_pkg(ctx, argv[1])) == NULL)
 		return -1;
 
-	return download_pkg_sources(ctx, pkg);
+	return install_pkg_sources(ctx, pkg);
 }
