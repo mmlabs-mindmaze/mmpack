01fbf2251a34bd4e2e346a0ce457711fb378c1ac
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-02-14 17:41:33 +0100

    mmpack-build: use subparser for cmdline parsing
    Change-Id: I2624fd03e623ab24888dcc8236188e4ed50945bc

--- src/mmpack-build/mmpack_clean.py
+++ src/mmpack-build/mmpack_clean.py
@@ -8,26 +8,16 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from . workspace import Workspace
 
 
-CMD = 'clean'
-
-
 def add_parser_args(parser: ArgumentParser):
     parser.add_argument('--wipe',
                         action='store_true',
                         help='wipe all files including generated packages')
 
 
-def main(argv):
+def main(options):
     """
     helper to clean the mmpack generated files
     """
-    # pylint: disable=invalid-name
-    parser = ArgumentParser(description=__doc__,
-                            prog='mmpack-build ' + CMD,
-                            formatter_class=RawDescriptionHelpFormatter)
-    add_parser_args(parser)
-    options = parser.parse_args(argv[1:])
-
     if options.wipe:
         Workspace().wipe()
     else:
