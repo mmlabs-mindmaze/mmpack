169590f85bec37d4918d649bc690ac81c5a9c499
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-06-08 16:56:37 +0200

    mmpack-build: deprecates --prefix opt of pkg-create
    Option --prefix of mmpack-build pkg-create is deprecated in favor of
    --prefix global option of mmpack-build.
    
    Change-Id: I5cb5be9b01ae517a46d82f7aa5f47a535204511f

--- src/mmpack-build/mmpack_pkg_create.py
+++ src/mmpack-build/mmpack_pkg_create.py
@@ -5,20 +5,27 @@ through the tree for a mmpack folder, and use the containing folder as root
 directory.
 """
 
-from argparse import ArgumentParser, Namespace
+from argparse import Action, ArgumentParser, Namespace
 
+from .common import wprint
 from .mmpack_mksource import (add_parser_args as add_mksource_parser_argument,
                               call_foreach_srcpkg)
 from .prefix import new_mmpack_prefix_context
 from .src_package import SrcPackage
 
 
+class DeprecatedStoreAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        wprint(f'Option {option_string} of {parser.prog} is deprecated')
+        setattr(namespace, self.dest, values)
+
+
 def add_parser_args(parser: ArgumentParser):
     """Enrich supplied argument parser with pkg-create related arguments"""
     add_mksource_parser_argument(parser)
     parser.add_argument('-p', '--prefix',
-                        action='store', dest='prefix', type=str,
-                        help='prefix within which to work')
+                        action=DeprecatedStoreAction, dest='prefix', type=str,
+                        help='prefix within which to work (DEPRECATED)')
     parser.add_argument('--skip-build-tests',
                         action='store_true', dest='skip_tests',
                         help='indicate that build tests must not be run')
