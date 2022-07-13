bd54584ea3540e5576a7cac35d47dfb121cd25b6
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-07-13 22:43:17 +0200

    mmpack: exec in prefix becomes platform specific
    Move platform specific source code related to mount prefix and execution
    in prefix in dedicated subdirs.
    
    Change-Id: I5c943add7cf2862963ce25c2f8cc525bbc623407

---
+++ src/mmpack/mount-prefix-linux/exec-in-prefix.c
@@ -0,0 +1,47 @@
+/*
+ * @mindmaze_header@
+ */
+#if defined (HAVE_CONFIG_H)
+# include <config.h>
+#endif
+
+#include <mmlib.h>
+#include <mmsysio.h>
+
+#include "context.h"
+
+#include "exec-in-prefix.h"
+
+
+static
+char* get_mount_prefix_bin(void)
+{
+	return LIBEXECDIR "/mmpack/mount-mmpack-prefix" EXEEXT;
+}
+
+
+LOCAL_SYMBOL
+int exec_in_prefix(const char* prefix, const char* argv[], int no_prefix_mount)
+{
+	char** new_argv;
+	int nargs;
+
+	// Count number of element in argv
+	for (nargs = 0; argv[nargs] != NULL; nargs++);
+
+	// new_argv contains: 1 command, 1 prefix, nargs arguments from
+	// argv and one terminating NULL => length = nargs+3
+	new_argv = alloca((nargs+3) * sizeof(*new_argv));
+	new_argv[0] = get_mount_prefix_bin();
+	new_argv[1] = (char*)prefix;
+
+	// args is terminated by NULL (either from argv or default_argv) and
+	// this is not counted in nargs, thus if we copy nargs+1 element
+	// from args, the NULL termination will be added to new_argv
+	memcpy(new_argv+2, argv, (nargs+1) * sizeof(*argv));
+
+	if (no_prefix_mount)
+		new_argv = (char**)argv;
+
+	return mm_execv(new_argv[0], 0, NULL, 0, new_argv, NULL);
+}
