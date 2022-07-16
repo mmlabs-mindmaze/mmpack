e50a266937ef5c048be5d769adece75561845d71
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-07-16 15:23:10 +0200

    mmpack: create mmpack-mount-prefix.dll
    Change-Id: I490b6e94d4909277379e1793f7a4dcb5a4e940fe

---
+++ src/mmpack/mount-prefix-nt/mount-helper.c
@@ -0,0 +1,281 @@
+/*
+ * @mindmaze_header@
+ */
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+
+#include <windows.h>
+
+#include "detours/detours.h"
+
+#include "internal-ntdll.h"
+#include "mmpack-mount-prefix.h"
+#include "mount-helper.h"
+
+
+static const GUID mmpack_guid = {
+	.Data1 = 0xadbc636a, 
+	.Data2 = 0x353b,
+	.Data3 = 0x4250,
+	.Data4 = {0xa0, 0xc1, 0x93, 0x3d, 0x29, 0xfc, 0xd2, 0xb7},
+};
+
+struct devmap {
+	HANDLE dir;
+	HANDLE drive;
+	DWORD size;
+	WCHAR dll_path[];
+};
+
+
+static struct devmap* prefix_devmap;
+
+
+/*****************************************************************************
+ *                       Memory allocation without using CRT                 *
+ *****************************************************************************/
+static HANDLE heap_hnd = INVALID_HANDLE_VALUE;
+
+static
+void* mem_alloc(size_t size)
+{
+	if (heap_hnd == INVALID_HANDLE_VALUE)
+		heap_hnd = GetProcessHeap();
+
+	return HeapAlloc(heap_hnd, 0, size);
+}
+
+
+static
+void mem_free(void* ptr)
+{
+	if (ptr)
+		HeapFree(heap_hnd, 0, ptr);
+}
+
+
+/*****************************************************************************
+ *                             NT path helpers                               *
+ *****************************************************************************/
+
+#define FILE_SHARE_ALL (FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE)
+
+/**
+ * get_nt_path() - return NT path on allocated string
+ * @path:       path in UTF-16
+ *
+ * Return: allocated string holding the NT path. Free with mem_free().
+ */
+static
+WCHAR* get_nt_path(const WCHAR* path)
+{
+	HANDLE hnd;
+	DWORD len;
+	WCHAR* ntpath = NULL;
+	SECURITY_ATTRIBUTES sec_attrs = {
+		.nLength = sizeof(SECURITY_ATTRIBUTES),
+		.bInheritHandle = FALSE,
+	};
+
+	// Open the file to get the final path
+	hnd = CreateFileW(path, READ_CONTROL, FILE_SHARE_ALL, &sec_attrs,
+	                  OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+	if (hnd == INVALID_HANDLE_VALUE)
+		return NULL;
+
+	// Get the len of path and allocate needed size
+	if (!(len = GetFinalPathNameByHandleW(hnd, NULL, 0, VOLUME_NAME_NT))
+	   || !(ntpath = mem_alloc((len+1) * sizeof(WCHAR))))
+		goto exit;
+
+	GetFinalPathNameByHandleW(hnd, ntpath, len+1, VOLUME_NAME_NT);
+
+exit:
+	CloseHandle(hnd);
+	return ntpath;
+}
+
+
+static
+WCHAR* get_dll_nt_path(HANDLE dllhnd)
+{
+	int len;
+	WCHAR *dllpath, *ntpath;
+
+	// Get the len of path and allocate needed size
+	if (!(len = GetModuleFileNameW(dllhnd, NULL, 0))
+	   || !(dllpath = mem_alloc((len+1) * sizeof(WCHAR))))
+		return NULL;
+
+	GetModuleFileNameW(dllhnd, dllpath, len+1);
+	ntpath = get_nt_path(dllpath);
+
+	mem_free(dllpath);
+	return ntpath;
+}
+
+
+/**
+ * path_size() - Get size in bytes of WCHAR path
+ * @path:       null terminated path in WCHAR
+ *
+ * Return: size in byte of path including the terminating nul.
+ */
+static
+size_t path_size(const WCHAR* path)
+{
+	int len;
+
+	// Get length of path (excluding terminating NUL)
+	for (len = 0; !path[len]; len++);
+
+	return (len + 1) * sizeof(*path);
+}
+
+
+
+static
+int apply_devmap(HANDLE target_proc)
+{
+	return NtSetInformationProcess(target_proc, ProcessDeviceMap,
+	                               &prefix_devmap->dir,
+	                               sizeof(prefix_devmap->dir));
+}
+
+
+LOCAL_SYMBOL
+int prefix_mount_setup_child(PROCESS_INFORMATION* procinfo)
+{
+	FARPROC pfn;
+	HANDLE child_proc = procinfo->hProcess;
+	struct devmap* child_dm;
+	HANDLE child_hnds[2] = {INVALID_HANDLE_VALUE, INVALID_HANDLE_VALUE};
+	HANDLE curr_proc = GetCurrentProcess();
+
+	if (!prefix_devmap)
+		return 0;
+
+	if (apply_devmap(child_proc))
+		return -1;
+
+	// Copy prefix_devmap to child as payload
+	child_dm = DetourCopyPayloadToProcessEx(child_proc,
+	                                        &mmpack_guid,
+                                                prefix_devmap,
+	                                        prefix_devmap->size);
+	if (!child_dm
+	    || !DuplicateHandle(curr_proc, prefix_devmap->dir,
+	                        child_proc, child_hnds,
+	                        0, FALSE, DUPLICATE_SAME_ACCESS)
+	    || !DuplicateHandle(curr_proc, prefix_devmap->drive,
+	                        child_proc, child_hnds+1,
+	                        0, FALSE, DUPLICATE_SAME_ACCESS)
+	    || WriteProcessMemory(child_proc, &child_dm->dir, child_hnds,
+	                          sizeof(child_hnds), NULL))
+		return -1;
+
+	// Queue LoadLibrary(dll_pathname) in remote process
+	pfn = GetProcAddress(GetModuleHandleW(L"kernel32"), "LoadLibraryW");
+	QueueUserAPC(procinfo->hThread, pfn, (ULONG_PTR)(child_dm->dll_path));
+
+	return 0;
+}
+
+
+LOCAL_SYMBOL
+void prefix_mount_process_startup(void)
+{
+	prefix_devmap = DetourFindPayloadEx(&mmpack_guid, NULL);
+}
+
+
+static
+int init_prefix_devmap(HANDLE dir, HANDLE drive)
+{
+	size_t map_sz, dllpath_sz;
+	WCHAR* dllpath;
+	HMODULE dllhnd;
+	int rv = -1;
+
+	// Get handle of current dll and length of its pathname
+	GetModuleHandleExW((GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS
+			     | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT),
+	                   (LPCWSTR)&prefix_devmap, &dllhnd);
+	dllpath = get_dll_nt_path(dllhnd);
+	if (!dllpath)
+		return -1;
+
+	// Compute needed size for holding the devmap struct
+	dllpath_sz = path_size(dllpath);
+	map_sz = sizeof(*prefix_devmap) + dllpath_sz;
+
+	// Allocate local structure devmap
+	prefix_devmap = mem_alloc(map_sz);
+	if (!prefix_devmap)
+		goto exit;
+
+	prefix_devmap->dir = dir;
+	prefix_devmap->drive = drive;
+	prefix_devmap->size = map_sz;
+	CopyMemory(prefix_devmap->dll_path, dllpath, dllpath_sz);
+	rv = 0;
+
+exit:
+	mem_free(dllpath);
+	return rv;
+}
+
+
+static
+int prefix_mount_setup(WCHAR* prefix_path)
+{
+	HANDLE curr_proc = GetCurrentProcess();
+	OBJECT_ATTRIBUTES attr;
+	UNICODE_STRING name, target;
+	HANDLE dir = INVALID_HANDLE_VALUE;
+	HANDLE drive = INVALID_HANDLE_VALUE;
+	WCHAR* nt_prefix_path;
+
+	// Transform the supplied prefix into an abspath relative to NT device
+	// (insensitive to device map)
+	nt_prefix_path = get_nt_path(prefix_path);
+	if (!nt_prefix_path)
+		return -1;
+
+	// Create the directory holding the prefix device map
+	RtlInitUnicodeString(&name, L"\\??\\mmpack-prefix");
+	InitializeObjectAttributes(&attr, &name,
+	                           OBJ_CASE_INSENSITIVE, NULL, NULL);
+	if (NtCreateDirectoryObject(&dir, DIRECTORY_ALL_ACCESS, &attr))
+		goto failure;
+
+	// Create drive letter in devicemap
+	RtlInitUnicodeString(&name, L"M:");
+	InitializeObjectAttributes(&attr, &name, OBJ_CASE_INSENSITIVE, dir, NULL);
+	RtlInitUnicodeString(&target, nt_prefix_path);
+	if (NtCreateSymbolicLinkObject(&drive, SYMBOLIC_LINK_ALL_ACCESS,
+	                               &attr, &target)
+	    || init_prefix_devmap(dir, drive))
+		goto failure;
+
+	mem_free(nt_prefix_path);
+	return apply_devmap(curr_proc);
+
+failure:
+	if (drive != INVALID_HANDLE_VALUE)
+		CloseHandle(drive);
+
+	if (dir != INVALID_HANDLE_VALUE)
+		CloseHandle(dir);
+
+	mem_free(nt_prefix_path);
+	return -1;
+}
+
+
+API_EXPORTED
+const struct mount_mmpack_dispatch dispatch_table = {
+	.setup = prefix_mount_setup,
+};
