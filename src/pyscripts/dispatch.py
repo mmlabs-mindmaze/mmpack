ce56fb2ca23f14f35527a45e1a0d33321ab5aade
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-10-08 13:53:25 +0200

    pyscripts: centralize startup from a __main__.py
    Regroup script startup of depends.py, dispatch.py and provides.py into a
    single __main__.py script. This allows to regroup all modules into a
    single package which will be needed later to refactor some part of the
    python scripts codebase.
    
    Change-Id: I0f49bad92d0382e4c621201952d8b8650ab09f08

--- src/pyscripts/dispatch.py
+++ src/pyscripts/dispatch.py
@@ -6,14 +6,12 @@ python public package dispatch
 
 import json
 import sys
-from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from functools import cache
 from os.path import abspath, basename
 from platform import system
 from sysconfig import get_config_var
-from typing import Dict, Set
+from typing import Dict, Iterable, Set
 
-from astroid import MANAGER
 from astroid.modutils import (file_info_from_modpath, is_namespace,
                               modpath_from_file_with_callback)
 
@@ -61,46 +59,15 @@ def get_root_modname(pyfile: str) -> str:
     return root_modname
 
 
-def parse_options():
-    """
-    parse options
-    """
-    parser = ArgumentParser(description=__doc__,
-                            formatter_class=RawDescriptionHelpFormatter)
-    parser.add_argument('--site-path', dest='site_paths', type=str, nargs='?',
-                        action='append', default=[],
-                        help='path of python site-packages or folder '
-                        'containing python package')
-    parser.add_argument('infile', type=str, nargs='?')
-
-    return parser.parse_args()
-
-
-def main():
+def run_dispatch(input_files: Iterable[str]):
     """
     python_dispatch utility entry point
     """
-    options = parse_options()
-
-    MANAGER.always_load_extensions = True
-
-    # If site path folders are specified, add them to sys.path so that astroid
-    # resolve the imports properly
-    for sitedir in options.site_paths:
-        sys.path.insert(0, abspath(sitedir))
-
-    with open(options.infile) as input_stream:
-        pyfiles = [f.strip() for f in input_stream]
-
     pypkgs: Dict[str, Set[str]] = {}
-    for pyfile in pyfiles:
+    for pyfile in input_files:
         modname = get_root_modname(pyfile)
         pypkgs.setdefault(modname, set()).add(pyfile)
 
     # Return results as JSON dict on stdout
     json.dump({k: list(sorted(v)) for k, v in pypkgs.items()},
               fp=sys.stdout, indent='    ', separators=(',', ': '))
-
-
-if __name__ == '__main__':
-    main()
