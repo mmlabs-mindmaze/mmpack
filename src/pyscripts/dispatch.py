1efcb0ce3f158105a9a2fe7bc9d0ccf473aaf2d5
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-10-08 15:52:56 +0200

    pyscripts: factorize is_namespace_[pkg|mod]
    create utils.py module and move is_namespace_pkg there.
    
    Change-Id: I00477211f8d2187d2b4f7702ad2d5facbf07f26f

--- src/pyscripts/dispatch.py
+++ src/pyscripts/dispatch.py
@@ -6,14 +6,14 @@ python public package dispatch
 
 import json
 import sys
-from functools import cache
 from os.path import abspath, basename
 from platform import system
 from sysconfig import get_config_var
 from typing import Dict, Iterable, Set
 
-from astroid.modutils import (file_info_from_modpath, is_namespace,
-                              modpath_from_file_with_callback)
+from astroid.modutils import modpath_from_file_with_callback
+
+from .utils import is_namespace_pkg
 
 
 LIMITED_API_EXTENSION_SUFFIX = '.abi3.so'
@@ -25,17 +25,6 @@ COMPILED_EXT_SUFFIXES = (
     get_config_var('EXT_SUFFIX'),
     LIMITED_API_EXTENSION_SUFFIX,
 )
-
-
-@cache
-def is_namespace_mod(modname: str) -> bool:
-    """reports true if modname is a PEP420 namespace package"""
-    try:
-        is_ns = is_namespace(file_info_from_modpath(modname.split('.')))
-    except ImportError:
-        is_ns = False
-
-    return is_ns
 
 
 def get_root_modname(pyfile: str) -> str:
@@ -53,7 +42,7 @@ def get_root_modname(pyfile: str) -> str:
     # Find first parent package that is not a namespace
     for depth in range(1, len(modpath)+1):
         root_modname = '.'.join(modpath[:depth])
-        if not is_namespace_mod(root_modname):
+        if not is_namespace_pkg(root_modname):
             break
 
     return root_modname
