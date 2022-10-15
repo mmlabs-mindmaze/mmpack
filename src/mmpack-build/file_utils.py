83a153ee90e1a324b0f977806a7e2663d74f9e07
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-10-15 07:16:44 +0200

    mmpack-build: filetype() recognize zip archive
    Change-Id: I60175e3f700364fd48972b518a6660da20506442

--- src/mmpack-build/file_utils.py
+++ src/mmpack-build/file_utils.py
@@ -46,6 +46,8 @@ def filetype(filename):
             return 'pe'
         if magic[:7] == b'!<arch>':
             return 'ar'
+        if magic[:4] == b'\x50\x4B\x03\x04':
+            return 'zip'
         if magic[:2] == b'#!':
             # the file contains a shebang. So the complete first line of file
             # and parse the interpreter. Search for end of line is performed in
