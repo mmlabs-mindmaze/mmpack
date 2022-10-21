8cb5c0c460ac0f0bcfac2e87fd13a9a26177c769
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-10-21 21:33:12 +0200

    pyscripts: Vendor astroid 2.5.1 as submodule
    Change-Id: I20e6cb9a493bc0048f0506626c5547ff3307b4fb

---
+++ src/pyscripts/__init__.py
@@ -0,0 +1,7 @@
+# @mindmaze_header@
+# pylint: disable=missing-module-docstring
+
+import sys
+from os.path import dirname, join as join_path
+
+sys.path.insert(0, join_path(dirname(__file__), '_vendor'))
