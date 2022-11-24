a5dd0ca74233fa68902b34c26e7b1a4f06d8c921
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-11-24 21:42:35 +0100

    mmpack: sha_file_compute usable
    Change-Id: I63e9086638ed7a99022c6a6f8bc3e73077af6789

--- src/mmpack/crypto.h
+++ src/mmpack/crypto.h
@@ -5,6 +5,10 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
 
+#include <nettle/sha2.h>
+#include <stdalign.h>
+#include <stdint.h>
+
 #include "mmstring.h"
 
 
@@ -14,6 +18,11 @@
 /* string of header and SHA-256 in hexa (\0 NOT incl.) */
 #define SHA_HEXSTR_LEN (SHA_HDRLEN + 32*2)
 
+struct sha_digest {
+	uint8_t data[SHA256_DIGEST_SIZE];
+};
+
+int sha_file_compute(struct sha_digest* digest, const char* filename);
 int sha_compute(mmstr* hash, const mmstr* filename, int follow);
 int check_hash(const mmstr* sha, const mmstr* filename);
 
