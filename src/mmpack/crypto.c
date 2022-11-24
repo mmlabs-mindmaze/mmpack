4635b8edfb9b4db912dc43edbacb130eb1e9ea82
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-11-24 21:42:35 +0100

    mmpack: introduce digest_t as binary SHA256 hash
    This adds a new type digest_t exposed in crypto.h representing a SHA256
    hash in binary. To manipulate it, internals of crypto function are
    adapted to this type and function, sha_file_compute(), to generate the
    SHA256 of a file from its path is provided. In addition to this,
    functions performing conversion from and to string buffer are provided
    as well.
    
    Change-Id: I63e9086638ed7a99022c6a6f8bc3e73077af6789

--- src/mmpack/crypto.c
+++ src/mmpack/crypto.c
@@ -25,54 +25,93 @@
  *                                                                        *
  **************************************************************************/
 /**
- * conv_to_hexstr() - convert byte array into hexadecimal string
+ * hexstr_from_digest() - convert binary sha256 hash into hexadecimal string
  * @hexstr:     output string, must be (2*@len) long
- * @data:       byte array to convert
- * @len:        length of @data
+ * @digest:     pointer to structure holding the SHA256 digest.
  *
- * This function generates the hexadecimal string representation of a byte
- * array.
+ * This function generates the hexadecimal string representation of a binary
+ * SHA256 digest.
  *
  * Return: length of the string written in @hexstr
  */
-static
-int conv_to_hexstr(char* hexstr, const unsigned char* data, size_t len)
+LOCAL_SYMBOL
+int hexstr_from_digest(char* hexstr, const digest_t* digest)
 {
 	const char hexlut[] = "0123456789abcdef";
-	unsigned char d;
+	uint8_t d;
 	size_t i;
 
-	for (i = 0; i < len; i++) {
-		d = data[i];
+	for (i = 0; i < sizeof(digest->u8); i++) {
+		d = digest->u8[i];
 		hexstr[2*i + 0] = hexlut[(d >> 4) & 0x0F];
 		hexstr[2*i + 1] = hexlut[(d >> 0) & 0x0F];
 	}
 
-	return 2*len;
+	return 2*sizeof(digest->u8);
 }
 
 
 /**
- * sha_fd_compute() - compute SHA256 hash of an open file
- * @hash:       mmstr* buffer receiving the hexadecimal form of hash. The
- *              pointed buffer must be SHA_HEXSTR_LEN long.
- * @fd:         file descriptor of a file opened for reading
+ * digest_from_hexstr() - convert hexadecimal string to digest
+ * @digest:     pointer to digest to fill
+ * @hexstr:     string chunk pointing a hexadecimal SHA256 value (64 bytes)
  *
- * The computed hash is stored in hexadecimal as a mmstr* string in @hash whose
- * length must be at least SHA_HEXSTR_LEN long, as reported by mmstr_maxlen().
+ * Return: 0 in case of success, -1 if @hexstr points to an invalid value.
+ */
+LOCAL_SYMBOL
+int digest_from_hexstr(digest_t* digest, struct strchunk hexstr)
+{
+	int i;
+	uint8_t d;
+	char c;
+
+	if (hexstr.len != SHA_HEXLEN)
+		goto error;
+
+	for (i = 0; i < SHA_HEXLEN; i++) {
+		c = hexstr.buf[i];
+		if (c >= '0' && c <= '9')
+			d = c - '0';
+		else if (c >= 'a' && c <= 'f')
+			d = c - 'a' + 10;
+		else if (c >= 'A' && c <= 'F')
+			d = c - 'A' + 10;
+		else
+			goto error;
+
+		if (i % 2 == 0)
+			digest->u8[i / 2] = d << 4;
+		else
+			digest->u8[i / 2] += d;
+	}
+	return 0;
+
+error:
+	return mm_raise_error(EINVAL, "invalid hexstr (l%i) (%.*s) argument",
+	                      hexstr.len, hexstr.len, hexstr.buf);
+}
+
+
+/**
+ * sha_file_compute() - compute SHA256 hash of a file
+ * @digest:     pointer to structure holding the SHA256 digest.
+ * @path:       path to file to hash
  *
  * Return: 0 in case of success, -1 if a problem of file reading has been
  * encountered.
  */
-static
-int sha_fd_compute(char* hash, int fd)
+LOCAL_SYMBOL
+int sha_file_compute(digest_t* digest, const char* path)
 {
-	uint8_t md[SHA256_DIGEST_SIZE], data[HASH_UPDATE_SIZE];
+	uint8_t data[HASH_UPDATE_SIZE];
 	struct sha256_ctx ctx;
 	ssize_t rsz;
-	int rv = 0;
+	int fd, rv = 0;
 
 	sha256_init(&ctx);
+	fd = mm_open(path, O_RDONLY, 0);
+	if (fd < 0)
+		return -1;
 
 	do {
 		rsz = mm_read(fd, data, sizeof(data));
@@ -84,49 +123,18 @@ int sha_fd_compute(char* hash, int fd)
 		sha256_update(&ctx, rsz, data);
 	} while (rsz > 0);
 
-	sha256_digest(&ctx, sizeof(md), md);
-
-	conv_to_hexstr(hash, md, sizeof(md));
-
-	return rv;
-}
-
-
-static
-int sha_regfile_compute(mmstr* hash, const char* path, int with_prefix)
-{
-	int fd;
-	int rv = 0;
-	char* hexstr;
-
-	// If with_prefix is set, SHA_HDR_REG ("reg-") is prefixed in hash
-	if (with_prefix) {
-		memcpy(hash, SHA_HDR_REG, SHA_HDRLEN);
-		hexstr = hash + SHA_HDRLEN;
-		mmstr_setlen(hash, SHA_HEXSTR_LEN);
-	} else {
-		hexstr = hash;
-		mmstr_setlen(hash, SHA_HEXSTR_LEN - SHA_HDRLEN);
-	}
-
-	if ((fd = mm_open(path, O_RDONLY, 0)) < 0
-	    || sha_fd_compute(hexstr, fd)) {
-		rv = -1;
-	}
-
 	mm_close(fd);
+	sha256_digest(&ctx, sizeof(digest->u8), digest->u8);
 
 	return rv;
 }
 
 
 static
-int sha_symlink_compute(mmstr* hash, const char* path, size_t target_size)
+int sha_symlink_compute(digest_t* digest, const char* path, size_t target_size)
 {
-	uint8_t md[SHA256_DIGEST_SIZE];
 	struct sha256_ctx ctx;
 	char* buff;
-	int len;
 	int rv = -1;
 
 	buff = xx_malloca(target_size);
@@ -135,12 +143,7 @@ int sha_symlink_compute(mmstr* hash, const char* path, size_t target_size)
 
 	sha256_init(&ctx);
 	sha256_update(&ctx, target_size-1, (uint8_t*)buff);
-	sha256_digest(&ctx, sizeof(md), md);
-
-	// Convert sha into hexadecimal and with "sym-" prefixed
-	memcpy(hash, SHA_HDR_SYM, SHA_HDRLEN);
-	len = conv_to_hexstr(hash + SHA_HDRLEN, md, sizeof(md));
-	mmstr_setlen(hash, len + SHA_HDRLEN);
+	sha256_digest(&ctx, sizeof(digest->u8), digest->u8);
 
 	rv = 0;
 
@@ -178,9 +181,11 @@ exit:
 LOCAL_SYMBOL
 int sha_compute(mmstr* hash, const mmstr* filename, int follow)
 {
+	digest_t digest;
 	int rv = 0;
-	int needed_len, with_prefix;
+	int needed_len, with_prefix, offset, len;
 	struct mm_stat st;
+	const char* prefix;
 
 	with_prefix = !follow;
 
@@ -195,13 +200,22 @@ int sha_compute(mmstr* hash, const mmstr* filename, int follow)
 	}
 
 	if (S_ISREG(st.mode)) {
-		rv = sha_regfile_compute(hash, filename, with_prefix);
+		rv = sha_file_compute(&digest, filename);
+		prefix = SHA_HDR_REG;
 	} else if (S_ISLNK(st.mode)) {
-		rv = sha_symlink_compute(hash, filename, st.size);
+		rv = sha_symlink_compute(&digest, filename, st.size);
+		prefix = SHA_HDR_SYM;
 	} else {
 		rv = mm_raise_error(EINVAL, "%s is neither a regular file "
 		                    "or symlink", filename);
+		goto exit;
 	}
+
+	// Convert sha into hexadecimal and with prefix if needed
+	offset = with_prefix ? SHA_HDRLEN : 0;
+	memcpy(hash, prefix, offset);
+	len = hexstr_from_digest(hash + offset, &digest);
+	mmstr_setlen(hash, len + offset);
 
 exit:
 	if (rv == -1)
@@ -234,6 +248,30 @@ int check_hash(const mmstr* refsha, const mmstr* filename)
 		return -1;
 
 	if (!mmstrequal(sha, refsha)) {
+		mm_raise_error(EBADMSG, "bad SHA-256 detected %s", filename);
+		return -1;
+	}
+
+	return 0;
+}
+
+
+/**
+ * check_digest() - Check integrity of given file using digest
+ * @ref:      reference sha256 digest to compare against
+ * @filename: path of file whose hash must be computed
+ *
+ * Return: 0 if no issue has been found, -1 otherwise
+ */
+LOCAL_SYMBOL
+int check_digest(const digest_t* ref, const char* filename)
+{
+	digest_t sha;
+
+	if (sha_file_compute(&sha, filename))
+		return -1;
+
+	if (!digest_equal(&sha, ref)) {
 		mm_raise_error(EBADMSG, "bad SHA-256 detected %s", filename);
 		return -1;
 	}
