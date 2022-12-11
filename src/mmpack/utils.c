0b06a4b0ece7e31a075e3cbee59d826bdfee46d5
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-12-11 18:01:28 +0100

    mmpack: add save_file() and load_file()
    Change-Id: I2fd36ec7405f20d3ae9f26587a1f47657751fdb7

--- src/mmpack/utils.c
+++ src/mmpack/utils.c
@@ -756,11 +756,77 @@ exit:
 
 /**************************************************************************
  *                                                                        *
- *                        Compressed file handling                        *
+ *                        High level file handling                        *
  *                                                                        *
  **************************************************************************/
 
 #define BLOCK_SZ        8192
+#define NUM_ATTEMPT             10
+
+
+static
+int save_file(const char* path, const struct buffer* buff)
+{
+	int fd, rv, rlen;
+	const char* data = buff->base;
+	int remaining = buff->size;
+
+	fd = mm_open(path, O_WRONLY|O_CREAT|O_EXCL, 0666);
+	if (fd < 0)
+		return -1;
+
+	rv = 0;
+	while (remaining) {
+		rlen = mm_write(fd, data, remaining);
+		if (rlen < 0) {
+			rv = -1;
+			goto exit;
+		}
+
+		remaining -= rlen;
+		data += rlen;
+	}
+
+exit:
+	mm_close(fd);
+	return rv;
+}
+
+
+/**
+ * save_file_atomically() - save content of a buffer into a file
+ * @path:       path of the file to open
+ * @buff:       pointer to struct buffer that will hold the data to be written
+ *
+ * Return: 0 in case if success. Otherwise -1 is returned with error state set
+ * accordingly.
+ */
+LOCAL_SYMBOL
+int save_file_atomically(const mmstr* path, const struct buffer* buff)
+{
+	int flags, i, rv = -1;
+	mmstr* tmp_path = NULL;
+
+	tmp_path = mmstrcpy_cstr_realloc(NULL, path);
+
+	flags = mm_error_set_flags(MM_ERROR_SET, MM_ERROR_IGNORE);
+	for (i = 0; i < NUM_ATTEMPT; i++) {
+		tmp_path = mmstr_append_realloc(tmp_path, "~", 1);
+		rv = save_file(tmp_path, buff);
+		if (rv == 0)
+			break;
+
+		if (i >= NUM_ATTEMPT-1 || mm_get_lasterror_number() != EEXIST)
+			goto exit;
+	}
+	rv = mm_rename(tmp_path, path);
+
+exit:
+	mm_error_set_flags(flags, MM_ERROR_IGNORE);
+	mmstr_free(tmp_path);
+	return rv;
+}
+
 
 static
 int from_zlib_error(int zlib_errnum)
