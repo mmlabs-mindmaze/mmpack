23d45c77b8d632dbd8c31cf0e6987e7b2729a422
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-12-30 15:32:47 +0100

    mmpack: Introduce sumsha table
    Change-Id: I156cb1cfd7ac156d8a3c83ce4d5ca2b1aaa1ffc0

--- src/mmpack/sumsha.h
+++ src/mmpack/sumsha.h
@@ -6,11 +6,65 @@
 #define SUMSHA_H
 
 #include "binpkg.h"
+#include "indextable.h"
 #include "mmstring.h"
 #include "strlist.h"
 
 
+struct sumsha {
+	struct indextable idx;
+};
+
+
+struct sumsha_entry {
+	struct typed_hash hash;
+	struct mmstring path;
+};
+
+
+struct sumsha_iterator {
+	struct it_iterator idx_iter;
+};
+
+
+/**
+ * sumsha_first() - initialize a sumsha iterator and get first element
+ * @iter:       pointer to sumsha_iterator to use for the iteration
+ * @sumsha:     sumsha table to iterate.
+ *
+ * Return: pointer to first sumsha_entry element, NULL if the table is empty.
+ */
+static inline
+struct sumsha_entry* sumsha_first(struct sumsha_iterator* iter,
+                                  const struct sumsha* sumsha)
+{
+	struct it_entry* e;
+
+	e = it_iter_first(&iter->idx_iter, &sumsha->idx);
+	return e ? e->value : NULL;
+}
+
+
+/**
+ * sumsha_next() - get next sumsha element of iteration
+ * @iter:       pointer to initialized sumsha_iterator
+ *
+ * Return: pointer to next sumsha_entry element, NULL if end of table.
+ */
+static inline
+struct sumsha_entry* sumsha_next(struct sumsha_iterator* iter)
+{
+	struct it_entry* e;
+
+	e = it_iter_next(&iter->idx_iter);
+	return e ? e->value : NULL;
+}
+
+
 mmstr* sha256sums_path(const struct binpkg* pkg);
+void sumsha_init(struct sumsha* sumsha);
+int sumsha_load(struct sumsha* sumsha, const char* sumsha_path);
+void sumsha_deinit(struct sumsha* sumsha);
 int read_sha256sums(const mmstr* sha256sums_path,
                     struct strlist* filelist, struct strlist* hashlist);
 int read_sumsha_filelist(const char* sumsha_path, struct strlist* filelist);
