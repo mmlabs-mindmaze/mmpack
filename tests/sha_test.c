10f757daebc636703183913995f156cc74affb23
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2022-12-31 08:43:45 +0100

    mmpack: remove unused prefixed hash functions
    Change-Id: I59f7765b00b20720d7fc7aab4f7f3e5ec96147d4

--- tests/sha_test.c
+++ tests/sha_test.c
@@ -25,7 +25,6 @@ static
 struct {
 	size_t len;
 	char name[64];
-	char refhash[SHA_HEXSTR_LEN+1];
 	digest_t refdigest;
 } sha_cases[] = {
 	{.len =  0, .name = "zero_len"},
@@ -47,17 +46,17 @@ struct {
 	char refhash[SHA_HEXSTR_LEN+1];
 } symlink_cases[] = {
 	{.name = "a_link", .target = "zero_len",
-	 .refhash = "sym-6d712e7edfc58ec6ca9b28efd3a01175c7cdb4178921aab352ea0ec56b6cdab6"},
+	 .refhash = "6d712e7edfc58ec6ca9b28efd3a01175c7cdb4178921aab352ea0ec56b6cdab6"},
 	{.name = "b_link", .target = "small_file",
-	 .refhash = "sym-63425cb6d2a9304d3ed60ea1aa2b31a2c7804c98ddf31fb818a45a36433a2f1c"},
+	 .refhash = "63425cb6d2a9304d3ed60ea1aa2b31a2c7804c98ddf31fb818a45a36433a2f1c"},
 	{.name = "links/bdir/blink", .target = "../adir",
-	 .refhash = "sym-4714d47f0664d0e100b078e25fc66994ee7251f172062040dcadc4fe4cd85f20"},
+	 .refhash = "4714d47f0664d0e100b078e25fc66994ee7251f172062040dcadc4fe4cd85f20"},
 	{.name = "links/some dir/foo", .target = "to_non_existent",
-	 .refhash = "sym-88a3f6baef51ad2c2a21443bd3178d9f8bb7bbfcdd8b49146643272d1c572709"},
+	 .refhash = "88a3f6baef51ad2c2a21443bd3178d9f8bb7bbfcdd8b49146643272d1c572709"},
 	{.name = "links/some dir/bar", .target = "../adir/to_non_existent",
-	 .refhash = "sym-c238df1ae2bd59950b09421be20f3c2f1e7e1c4d04ae872b9704664178b86ad6"},
+	 .refhash = "c238df1ae2bd59950b09421be20f3c2f1e7e1c4d04ae872b9704664178b86ad6"},
 	{.name = "c_link", .target = "links/adir",
-	 .refhash = "sym-b5f07c53c8f582a56708b4cb455a997a043a56da57ea3d94d23d0fc1369fddce"},
+	 .refhash = "b5f07c53c8f582a56708b4cb455a997a043a56da57ea3d94d23d0fc1369fddce"},
 };
 #define NUM_SYMLINK_CASES       MM_NELEM(symlink_cases)
 
@@ -99,29 +98,6 @@ void create_rand_file(const char* filename, size_t len)
 
 
 /**
- * gen_ref_hash() - compute the reference hash with openssl cmdline tool
- * @hash:       buffer receiving the hash (must be HASH_HEXSTR_SIZE long)
- * filename:    path of the file to hash
- */
-static
-void gen_ref_hash(char* hash, const char* filename)
-{
-	char cmd[512];
-	FILE* fp;
-	int sha256_strlen = SHA_HEXSTR_LEN - SHA_HDRLEN;
-
-	memcpy(hash, SHA_HDR_REG, SHA_HDRLEN);
-
-	sprintf(cmd, "openssl sha256 -hex %s | cut -f2 -d' '", filename);
-	fp = popen(cmd, "r");
-	if (fread(hash + SHA_HDRLEN, sha256_strlen, 1, fp)) {}
-	pclose(fp);
-
-	hash[SHA_HEXSTR_LEN] = '\0';
-}
-
-
-/**
  * gen_ref_md() - compute the reference digest with openssl cmdline tool
  * @digest:     buffer receiving the digest
  * filename:    path of the file to hash
@@ -154,7 +130,6 @@ void sha_setup(void)
 		strcpy(filename, HASHFILE_DIR "/");
 		strcat(filename, sha_cases[i].name);
 		create_rand_file(filename, sha_cases[i].len);
-		gen_ref_hash(sha_cases[i].refhash, filename);
 		gen_ref_digest(&sha_cases[i].refdigest, filename);
 	}
 
@@ -184,36 +159,6 @@ void sha_cleanup(void)
  *                                                                        *
  **************************************************************************/
 STATIC_CONST_MMSTR(hashfile_dir, HASHFILE_DIR);
-
-
-START_TEST(hash_regfile)
-{
-	mmstr* hash = mmstr_alloca(SHA_HEXSTR_LEN);
-	mmstr* fullpath = mmstr_alloca(256);
-	const mmstr* filename = mmstr_alloca_from_cstr(sha_cases[_i].name);
-	const mmstr* refhash = mmstr_alloca_from_cstr(sha_cases[_i].refhash);
-
-	mmstr_join_path(fullpath, hashfile_dir, filename);
-
-	sha_compute(hash, fullpath);
-	ck_assert_str_eq(hash, refhash);
-}
-END_TEST
-
-
-START_TEST(hash_symlink)
-{
-	mmstr* hash = mmstr_alloca(SHA_HEXSTR_LEN);
-	mmstr* fullpath = mmstr_alloca(256);
-	const mmstr* filename = mmstr_alloca_from_cstr(symlink_cases[_i].name);
-	const char* refhash = symlink_cases[_i].refhash;
-
-	mmstr_join_path(fullpath, hashfile_dir, filename);
-
-	ck_assert(sha_compute(hash, fullpath) == 0);
-	ck_assert_str_eq(hash, refhash);
-}
-END_TEST
 
 
 START_TEST(binary_sha)
@@ -253,7 +198,7 @@ START_TEST(typed_hash_symlink)
 	struct typed_hash hash;
 	mmstr* fullpath = mmstr_alloca(256);
 	const mmstr* filename = mmstr_alloca_from_cstr(symlink_cases[_i].name);
-	const char* refhexstr = symlink_cases[_i].refhash + SHA_HDRLEN;
+	const char* refhexstr = symlink_cases[_i].refhash;
 	digest_t refdigest;
 
 	digest_from_hexstr(&refdigest, strchunk_from_cstr(refhexstr));
@@ -315,9 +260,7 @@ TCase* create_sha_tcase(void)
 	tc = tcase_create("sha");
 	tcase_add_unchecked_fixture(tc, sha_setup, sha_cleanup);
 
-	tcase_add_loop_test(tc, hash_regfile, 0, NUM_HASH_CASES);
 	tcase_add_loop_test(tc, binary_sha, 0, NUM_HASH_CASES);
-	tcase_add_loop_test(tc, hash_symlink, 0, NUM_SYMLINK_CASES);
 	tcase_add_loop_test(tc, typed_hash_regfile, 0, NUM_HASH_CASES);
 	tcase_add_loop_test(tc, typed_hash_symlink, 0, NUM_SYMLINK_CASES);
 	tcase_add_test(tc, digest_to_str);
