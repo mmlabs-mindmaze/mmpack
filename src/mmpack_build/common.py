b5c70177c75908a3114332181566f9b20688d913
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2023-01-12 20:43:39 +0100

    mmpack-build: add dll exec perm in tar only on win32
    Change-Id: I6f90a281d565d1e56345c841457f9c98f9329519

--- src/mmpack_build/common.py
+++ src/mmpack_build/common.py
@@ -483,8 +483,10 @@ def _reset_entry_attrs(tarinfo: tarfile.TarInfo):
     tarinfo.uname = tarinfo.gname = 'root'
     tarinfo.mtime = 0
 
-    if tarinfo.name.lower().endswith('.dll'):
-        tarinfo.mode = 0o755
+    if get_host_dist() == 'windows':
+        ext, _ = os.path.splitext(tarinfo.name)
+        if ext.lower() == '.dll':
+            tarinfo.mode = 0o755
 
     return tarinfo
 
