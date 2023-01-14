a330e170be40df3882dbd873d210c433d50d0ddc
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2023-01-14 11:17:12 +0100

    mmpack: add sumsha_get()
    Change-Id: I6e68067fc9d06677690f0661c1c0cfe0cdab6d1c

--- src/mmpack/sumsha.h
+++ src/mmpack/sumsha.h
@@ -62,6 +62,14 @@ struct sumsha_entry* sumsha_next(struct sumsha_iterator* iter)
 }
 
 
+static inline
+struct sumsha_entry* sumsha_get(const struct sumsha* sumsha, const mmstr* path)
+{
+	struct it_entry* entry = indextable_lookup(&sumsha->idx, path);
+	return entry ? entry->value : NULL;
+}
+
+
 mmstr* sha256sums_path(const mmstr* rootpath, const struct binpkg* pkg);
 void sumsha_init(struct sumsha* sumsha);
 int sumsha_load(struct sumsha* sumsha, const char* sumsha_path);
