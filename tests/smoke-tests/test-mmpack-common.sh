bc6d19865ea8d2724f0a1ec4d61d4f461cdb3315
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2023-01-24 00:29:49 +0100

    tests: run each mmpack smoke test in its own test dir
    Meson allows to run the tests in parallel. To avoid the working
    directory of the tests being modified or removed by another tests, each
    mmpack smoke test is run in its own dedicated test folder.
    
    This avoids possible failure of download smoke test (maybe other tests
    affected as well) which previously got its test directory being removed
    sometime while being run.
    
    Change-Id: I286f263597cdd01538dfc3ff8fbc421a0b95f0f0

--- tests/smoke-tests/test-mmpack-common.sh
+++ tests/smoke-tests/test-mmpack-common.sh
@@ -20,18 +20,18 @@ prepare_env()
 
 	alias mmpack=$BUILDDIR/src/mmpack/mmpack$EXEEXT
 
-	# initialisation of variables
-	PREFIX_TEST=$TESTSDIR/prefix
-	TEST_SRCDIR=$SRCDIR/tests/smoke-tests
-
 	# set the environment variables for this test
 	name="$(basename $0)"
-	PREFIX_TEST+=_${name%.*}
+	CURR_TESTDIR=${TESTSDIR}_${name%.*}
+
+	# initialisation of variables
+	PREFIX_TEST=$CURR_TESTDIR/prefix
+	TEST_SRCDIR=$SRCDIR/tests/smoke-tests
 
 	# set the environment properly
 	export MMPACK_PREFIX=$PREFIX_TEST
-	export XDG_CACHE_HOME=$TESTSDIR/xdg-cache
-	export TESTS_DATA_DIR=$TESTSDIR/tmp-data
+	export XDG_CACHE_HOME=$CURR_TESTDIR/xdg-cache
+	export TESTS_DATA_DIR=$CURR_TESTDIR/tmp-data
 
 	# prevent loading user global configuration
 	export XDG_CONFIG_HOME=/non-existing-dir
@@ -43,8 +43,7 @@ prepare_env()
 # clean the files and repositories necessary for the tests
 cleanup()
 {
-	rm -rf $PREFIX_TEST
-	rm -rf $TESTS_DATA_DIR
+	rm -rf $CURR_TESTDIR
 }
 
 assert-str-equal()
