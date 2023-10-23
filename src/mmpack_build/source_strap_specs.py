15555cab079711ba94804c26d791818b424758b3
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2023-10-23 13:18:03 +0200

    mmpack-build: Move SourceStrapSpecs in dedicated module
    Change-Id: Ie0d8b91cf2ef0693504f09fd1898d38a1ce0f1f2

---
+++ src/mmpack_build/source_strap_specs.py
@@ -0,0 +1,57 @@
+# @mindmaze_header@
+"""
+Representation of loaded sources-strap file
+"""
+
+import re
+from os.path import join as join_path
+from typing import Any, Dict, List, Optional
+
+import yaml
+from .common import Assert, get_name_version_from_srcdir
+
+
+def _multiple_replace(lookup: Dict[str, str], text: str) -> str:
+    # Create a regular expression from all of the dictionary keys
+    regex = re.compile('|'.join(lookup.keys()))
+
+    # For each match, look up the corresponding value in the dictionary
+    return regex.sub(lambda match: lookup[match.group(0)], text)
+
+
+class SourceStrapSpecs:
+    """source-strap config"""
+
+    def __init__(self, srcdir: str):
+        name, version = get_name_version_from_srcdir(srcdir)
+        lookup = {
+            '@MMPACK_NAME@': name,
+            '@MMPACK_VERSION@': version,
+        }
+        try:
+            path = join_path(srcdir, 'mmpack/sources-strap')
+            with open(path, encoding='utf-8') as stream:
+                content = _multiple_replace(lookup, stream.read())
+                specs = yaml.load(content, Loader=yaml.BaseLoader)
+        except FileNotFoundError:
+            specs = {}
+
+        self.depends: List[str] = specs.pop('depends', [])
+        self.upstream_method: Optional[str] = specs.pop('method', None)
+        self.upstream_url: Optional[str] = specs.pop('url', None)
+        self.patches: List[str] = specs.pop('patches', [])
+        self._opts: Dict[str, str] = specs
+
+        self._validate()
+
+    def get(self, *args) -> Any:
+        """Get optional value"""
+        return self._opts.get(*args)
+
+    def _validate(self):
+        if self.upstream_method:
+            if self.upstream_method not in ('git', 'tar'):
+                raise Assert('Invalid method ' + self.upstream_method)
+
+            if not self.upstream_url:
+                raise Assert('upstream method specified but url missing')
